8913bc340eaf73f8aa8eac99a7e6692c9ce80495
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2018-03-23 13:51:11 +0100

    Shorter store/index.js

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vuex from "vuex";
 import Vue from "vue";
 
 import nodes from "./testdata/nodes";
+import jobs from "./testdata/jobs";
 
 import createEnssimPlugin from "./createEnssimPlugin";
 
@@ -10,53 +11,18 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     nodes,
+    jobs,
     errors: []
   },
   getters: {
     nodestatus(state) {
-      return state.jsondata.nodes.map(node => ({
+      return state.nodes.map(node => ({
         ...node,
-        jobs: node.jobs || [],
-        usage: (node.jobs || []).length / (node.np || 1)
+        jobs: state.jobs(node.NodeName)
+      })).map(node => ({
+        ...node,
+        users: state.users(node.jobs)
       }));
-    },
-
-    queuestatus(state) {
-      let queues = {};
-      state.jsondata.nodes.forEach(node => {
-        if (!queues[node.properties]) {
-          queues[node.properties] = {
-            name: node.properties,
-            properties: node.properties,
-            state: {},
-            np: 0,
-            jobs: []
-          };
-        }
-
-        let queue = queues[node.properties];
-        queue.np += Number(node.np);
-
-        if (node.jobs) {
-          queue.jobs.splice(queue.jobs.length, 0, ...node.jobs);
-        }
-
-        node.state.split(",").forEach(state => {
-          queue.state[state] = (queue.state[state] || 0) + 1;
-        });
-      });
-
-      Object.keys(queues).map(key => queues[key])
-        .forEach(queue => {
-          queue.usage = queue.jobs.length / queue.np;
-          queue.state = Object.keys(queue.state)
-            .map(state => ({ state: state, count: queue.state[state] }))
-            .sort((a, b) => b.count - a.count)
-            .map(state => `${state.count}x ${state.state}`)
-            .join(" ");
-        });
-
-      return Object.values(queues);
     }
   },
   mutations: {
