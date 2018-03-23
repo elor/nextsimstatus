81bb9a6865183fa4403b2999ebe0dac4d5befe58
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2018-03-23 13:50:59 +0100

    Comprehensive Nodes.vue update: All new

--- src/components/Nodes.vue
+++ src/components/Nodes.vue
@@ -2,103 +2,138 @@
   <v-container fluid>
     <v-card>
       <v-card-title>
-        <v-select :items="groups" item-value="data" v-model="tabledata">
-        </v-select>
         <v-spacer></v-spacer>
-        <v-text-field append-icon="search" label="Search" single-line hide-details v-model="search"></v-text-field>
+        <v-text-field append-icon="search"
+                      label="Search"
+                      single-line
+                      hide-details
+                      v-model="search"></v-text-field>
       </v-card-title>
-
-      <v-data-table :headers="headers" :items="tabledata" item-key="name" :search="search" hide-actions disable-initial-sort>
-
+      <v-data-table :headers="headers"
+                    :items="fullNodes"
+                    :search="search"
+                    hide-actions>
         <template slot="items" slot-scope="props">
-          <tr @click="props.expanded = !props.expanded">
-            <td>{{props.item.name}}</td>
-            <td class="text-xs-center">
-              <v-progress-linear :value="100 * props.item.usage">
-                {{props.item.jobs.length}}/{{props.item.np}}
-              </v-progress-linear>
-            </td>
-            <td>{{props.item.state}}</td>
-            <td>{{props.item.properties}}</td>
-          </tr>
+          <td>{{props.item.NodeName}}</td>
+          <td>
+            <v-progress-circular :value="100*props.item.CPUAlloc/props.item.CPUTot"
+                                 :color="props.item.CPUAlloc == props.item.CPUTot ? 'light-blue' : 'green'" >
+            {{props.item.CPUAlloc}}
+            </v-progress-circular>
+          </td>
+          <td>{{props.item.jobs.join(' ')}}</td>
+          <td>{{props.item.users.join(' ')}}</td>
+          <td>
+            <v-progress-circular :value="100*props.item.CPULoad"
+                                 :color="props.item.CPULoad > 0.9 ? 'light-blue' : 'green'">
+            </v-progress-circular>
+          </td>
+          <td>{{props.item.Partitions}}</td>
+          <td>
+            {{capitalize(props.item.State)}}
+            <v-icon color="warning" v-if="isWarningState(props.item.State)">warning</v-icon>
+            <v-icon color="error" v-if="isFailState(props.item.State)">error</v-icon>
+          </td>
+          <td>{{props.item.BootTime}}</td>
         </template>
-
-        <v-alert slot="no-results" :value="true" color="error" icon="warning">
-          No search results for '{{search}}'.
-          <v-btn primary small color="primary" @click="search=''">reset</v-btn>
-        </v-alert>
-
-        <template slot="expand" slot-scope="props">
-          <v-card flat>
-            <v-card-text>{{props.item.name}}</v-card-text>
-          </v-card>
-        </template>
-
-        <v-alert slot="no-data" type="error" value="true">
-          No items to display
-        </v-alert>
-
       </v-data-table>
-
     </v-card>
+
   </v-container>
 </template>
 
 <script>
-  import { mapState, mapGetters } from "vuex";
+  import { mapState } from "vuex";
+
+  const warnstates = ["NoResp", "DRAIN", "FAILING", "MAINT", "POWER_UP"];
+  const failstates = ["DOWN", "ERROR", "FAIL", "POWER_DOWN"];
 
   export default {
     data() {
       return {
         headers: [
-          { text: "Name", value: "name" },
-          { text: "Usage", value: "usage" },
-          { text: "Status", value: "state" },
-          { text: "Queue", value: "properties" }
+          {
+            text: "Node",
+            align: "left",
+            sortable: true,
+            value: "NodeName"
+          },
+          {
+            text: "Usage",
+            align: "left",
+            sortable: true,
+            value: "CPUAlloc"
+          },
+          {
+            text: "Jobs",
+            align: "left",
+            sortable: false,
+            value: "jobs"
+          },
+          {
+            text: "Users",
+            align: "left",
+            sortable: false,
+            value: "users"
+          },
+          {
+            text: "CPU",
+            align: "left",
+            sortable: true,
+            value: "CPULoad"
+          },
+          {
+            text: "Partitions",
+            align: "left",
+            sortable: true,
+            value: "Partitions"
+          },
+          {
+            text: "State",
+            align: "left",
+            sortable: true,
+            value: "State"
+          },
+          {
+            text: "BootTime",
+            align: "left",
+            sortable: true,
+            value: "BootTime"
+          }
         ],
-        search: "",
-        tabledata: this.nodestatus
+        search: ""
       };
     },
     computed: {
-      ...mapState(["errors"]),
-      ...mapGetters(["nodestatus", "queuestatus"]),
-
-      groups() {
-        return [
-          {
-            text: "Nodes",
-            data: this.nodestatus
-          },
-          {
-            text: "Queues",
-            data: this.queuestatus
-          }];
+      ...mapState(["nodes"]),
       }
     },
-    created() {
-      this.tabledata = this.nodestatus;
+    methods: {
+      capitalize(word) {
+        switch (word.length) {
+          case 0:
+            return "";
+          case 1:
+            return word.toUpperCase();
+          default:
+            return `${word[0].toUpperCase() + word.slice(1).toLowerCase()}`;
+        }
+      },
+      isWarningState(state) {
+        return warnstates.includes(state);
+      },
+      isFailState(state) {
+        return failstates.includes(state);
+      },
+      jobs(nodeName) {
+        return nodeName === "sim02" ? ["12345", "12345", "12345", "12345", "12345", "12345", "12345", "12345", "12345", "12345", "12345", "12345", "12345", "12345", "12345", "12345", "12345", "12345", "12345", "12345", "12345", "12345", "12345", "97531"] : [];
+      },
+      users(jobs) {
+        return jobs.map(job => "e.lorenz").sort().filter((value, index, users) => users[index + 1] !== value);
+      }
     }
   };
 </script>
 
 <style scoped>
-
-  h1, h2 {
-    font-weight: normal;
-  }
-
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-
-  a {
-    color: #42b983;
-  }
 </style>
