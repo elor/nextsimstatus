9dcb9aaac02676dda7f6fbab46798d31ceaa95ca
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2018-03-23 15:21:12 +0100

    Add nodels.js, which reproduces the "noidels" xcat tool

---
+++ src/components/nodels.js
@@ -0,0 +1,59 @@
+import { range, flattenDeep, flatten, uniq, padStart } from "lodash";
+
+export function nodels(definition) {
+  let re = /([a-z0-9]+)(\[(\d+([,-]\d+)*)\])?/g;
+  let strictRe = /^([a-z0-9]+)(\[(\d+([,-]\d+)*)\])?(,([a-z0-9]+)(\[(\d+([,-]\d+)*)\])?)*$/;
+
+  if (!strictRe.test(definition)) {
+    return [];
+  }
+
+  const defs = matchAll(re, definition);
+
+  const nodes = flatten(defs.map(def => def2nums(def.numdef).map(num => `${def.base}${num}`)));
+
+  return uniq(nodes);
+}
+
+function matchAll(re, string) {
+  let matches = [];
+
+  while (true) {
+    let match = re.exec(string);
+    if (!match) {
+      break;
+    }
+    matches.push({ base: match[1], numdef: match[3] });
+  }
+
+  return matches;
+}
+
+function def2nums(def) {
+  if (!def) {
+    return [""];
+  }
+
+  const parts = def.split(",");
+  const nums = parts.map(part => {
+    const match = /^(\d+)-(\d+)$/.exec(part);
+    if (!match) {
+      return part;
+    }
+
+    // reproduce odd behavior of "nodels" command
+    if (match[1].length > match[2].length) {
+      return [];
+    }
+
+    const first = Number(match[1]);
+    const last = Number(match[2]);
+
+    const numbers = range(first, last + 1);
+    const digits = Math.min(match[1].length, match[2].length);
+
+    return numbers.map(num => padStart(num, digits, "0"));
+  });
+
+  return flattenDeep(nums);
+}
