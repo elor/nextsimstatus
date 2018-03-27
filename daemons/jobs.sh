dcc9887e8478cb0dc4f2b4bc52b35c65759ce812
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2018-03-27 09:07:05 +0200

    daemons/jobs.sh returns empty array on default

--- daemons/jobs.sh
+++ daemons/jobs.sh
@@ -4,7 +4,7 @@ set -e -u
 
 jobs=$(scontrol show jobs --oneliner)
 
-grep "No jobs" <<< "$jobs" &>/dev/null && exit 1
+grep "No jobs" <<< "$jobs" &>/dev/null && { echo '[]'; exit 0; }
 
 sed -e 's/\(\S\+\)=\(\S*\)/"\1": "\2",/g' -e 's/^\(.\+\),\s*$/{\1},/' -e '1s/^/[/' -e '$s/},$/}]/' <<< "$jobs"
 
