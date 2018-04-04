5c41d2f9bf8ba47cf95fcb5016ad7c6ba817784f
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2018-04-04 12:16:41 +0200

    Start timer progress circles at the top

--- src/App.vue
+++ src/App.vue
@@ -36,15 +36,21 @@
       <v-spacer></v-spacer>
 
       {{dates.now.toLocaleString()}}
-      <v-progress-circular class="noanimation" :value="100 * (dates.now - dates.jobs) / options.timeout"
+      <v-progress-circular class="noanimation"
+                           rotate="-90"
+                           :value="100 * (dates.now - dates.jobs) / options.timeout"
                            :color="dates.now - dates.jobs > options.timeout ? 'red' : 'light-blue'">
         J
       </v-progress-circular>
-      <v-progress-circular class="noanimation" :value="100 * (dates.now - dates.nodes) / options.timeout"
+      <v-progress-circular class="noanimation"
+                           rotate="-90"
+                           :value="100 * (dates.now - dates.nodes) / options.timeout"
                            :color="dates.now - dates.nodes > options.timeout ? 'red' : 'light-blue'">
         N
       </v-progress-circular>
-      <v-progress-circular class="noanimation" :value="100 * (dates.now - dates.users) / options.timeout"
+      <v-progress-circular class="noanimation"
+                           rotate="-90"
+                           :value="100 * (dates.now - dates.users) / options.timeout"
                            :color="dates.now - dates.users > options.timeout ? 'red' : 'light-blue'">
         U
       </v-progress-circular>
