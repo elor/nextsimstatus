904ffc0a5c462e71cb89e1e05ead44256ef1332c
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2018-04-17 10:09:11 +0200

    Split daemon into two for robustness

--- daemons/mainsim-daemon.sh
+++ daemons/mainsim-daemon.sh
@@ -4,45 +4,48 @@ set -e -u
 
 cd "$(dirname "$0")"
 
+if (( ${#@} != 1 )); then
+    cat <<EOF >&2
+Syntax: $(basename "$0") <DATASET>
+
+Where DATASET can be "nodes" or "jobs"
+EOF
+    exit 1
+fi
+
 host=mainsimweb.etit.tu-chemnitz.de
+dataset="$1"
 
-echo "preparing to connect to mqtt host $host"
-read -p "mqtt user [mqtt]: " mqtt_user
-read -sp "mqtt password: " mqtt_password
+case "$dataset" in
+    jobs|nodes)
+        ;;
+    *)
+        echo "DATASET must be one of \"nodes\" or \"jobs\", not \"$dataset\"" >&2
+        ;;
+esac
+
+mqtt_user=mqtt
+
+read -sp "password for $mqtt_user@$host: " mqtt_password
 echo
-
-[ -z "$mqtt_user" ] && mqtt_user=mqtt
 [ -z "$mqtt_password" ] && { echo "you must provide a password to the mqtt host"; exit 1; }
 
-old_nodes=""
-old_jobs=""
-last_jobs=0
-last_nodes=0
+old_data=""
+last_data=0
 interval=1
 
 while true; do
-    nodes_json="$(./slurm/nodes.sh)"
-    jobs_json="$(./slurm/jobs.sh)"
+    data_json="$(./slurm/$dataset.sh)"
 
     now=$(date +%s)
 
-    if [ "$nodes_json" != "$old_nodes" ] || (( now >= last_nodes + interval )); then
-        mosquitto_pub -h "$host" -u "$mqtt_user" -P "$mqtt_password" -t "slurm/nodes" -s << EOF
-$nodes_json
+    if [ "$data_json" != "$old_data" ] || (( now >= last_data + interval )); then
+        mosquitto_pub -h "$host" -u "$mqtt_user" -P "$mqtt_password" -t "slurm/$dataset" -s << EOF
+$data_json
 EOF
-        old_nodes="$nodes_json"
-        last_nodes=$now
-        echo "nodes $(date)"
+        old_data="$data_json"
+        last_data=$now
+        echo "$dataset $(date)"
     fi
-
-    if [ "$jobs_json" != "$old_jobs" ] || (( now >= last_jobs + interval)); then
-        mosquitto_pub -h "$host" -u "$mqtt_user" -P "$mqtt_password" -t "slurm/jobs" -s << EOF
-$jobs_json
-EOF
-        old_jobs="$jobs_json"
-        last_jobs=$now
-        echo "jobs $(date)"
-    fi
-
 done
 
--- daemons/slurm/jobs.sh
+++ daemons/slurm/jobs.sh
@@ -7,4 +7,3 @@ jobs=$(scontrol show jobs --oneliner)
 grep "No jobs" <<< "$jobs" &>/dev/null && { echo '[]'; exit 0; }
 
 sed -e 's/\(\S\+\)=\(\S*\)/"\1": "\2",/g' -e 's/^\(.\+\),\s*$/{\1},/' -e '1s/^/[/' -e '$s/},$/}]/' <<< "$jobs"
-
