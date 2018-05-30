013fcb0675bda476f921ada8e6ac48a6ca4e5e45
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2018-05-30 17:27:16 +0200

    Elaborate on examples

--- src/assets/periodinator-examples.js
+++ src/assets/periodinator-examples.js
@@ -2,16 +2,17 @@ export default [
   "Und so schreiten wir ohne Pause sicheren Fußes gen Promotion",
   "SimCrew",
   "In Cooperation  with Prof Schreiber",
-  "Auswerten ohne Sinn und Verstand",
+  "Physik ohne Sinn und Verstand",
   "Periodyctionary",
   "This is one superfine page in this universe",
   "Arsen\nCer\nKupfer\nKrypton\nNiob\nNeon\nOganeson\nPlatin\nTitan\nTenness\nUran\nXenon",
   "Carbon\nNeon\nSilicon\nPhosphorus\nCopper\nArsenic\nKrypton\nSilver\nXenon\nBismuth\nAstatine\nTenessine\nOganesson",
-  "This site is huge brainwaste",
+  "This site is huge waste of brains",
   "Haufen\nKaufen\nLaufen\nRaufen\nSaufen\nTaufen",
   "Hier Beichten",
   "Pausenfunktionsseite",
   "Brokkoliauflauf",
   "I watch rainbows",
-  "Find more unusual sentences"
+  "Find more unusual sentences",
+  "This page is of no real use"
 ];
