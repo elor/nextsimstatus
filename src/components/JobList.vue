0236c44e979b44f6dc5671dd2114ddec60814d31
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2018-06-05 16:00:10 +0200

    Extract JobList from Jobs

---
+++ src/components/JobList.vue
@@ -0,0 +1,113 @@
+<template>
+  <v-card>
+    <v-card-title>
+      {{title}}
+      <v-spacer></v-spacer>
+      <v-flex xs="12" md="6">
+        <v-text-field append-icon="search"
+                      label="Search"
+                      single-line
+                      hide-details
+                      v-model="search"></v-text-field>
+      </v-flex>
+    </v-card-title>
+    <v-data-table :headers="headers"
+                  :items="items"
+                  :search="search"
+                  hide-actions
+                  disable-initial-sort>
+      <template slot="items" slot-scope="props">
+        <td><router-link :to="`/jobs/${props.item.JobId}`">{{props.item.JobId}}</router-link></td>
+        <td><router-link :to="`/users`">{{props.item.UserName}}</router-link></td>
+        <td>{{props.item.ArrayTaskId}}</td>
+        <td>{{props.item.JobName}}</td>
+        <td>{{capitalize(props.item.JobState)}}</td>
+        <td>
+          <span v-for="node in props.item.NodeNames" :key="node">
+            <router-link :to="`/nodes`">{{node}}</router-link>
+            &nbsp;
+          </span>
+        </td>
+        <td>{{props.item.NumCPUs}}</td>
+        <td>{{props.item.RunTime}}</td>
+        <td>
+          <time :datetime="props.item.StartTime === 'Unknown' ? props.item.SubmitTime : props.item.StartTime">
+            {{(props.item.StartTime === "Unknown" ? props.item.SubmitTime : props.item.StartTime).replace("T", " ")}}
+          </time>
+        </td>
+      </template>
+    </v-data-table>
+  </v-card>
+</template>
+
+<script>
+import { capitalize } from "../utils/capitalize";
+
+export default {
+  props: ["items", "title"],
+  data() {
+    return {
+      headers: [
+        {
+          text: "JobID",
+          align: "left",
+          sortable: true,
+          value: "JobId"
+        },
+        {
+          text: "User",
+          align: "left",
+          sortable: true,
+          value: "UserName"
+        },
+        {
+          text: "ArrayTaskId",
+          align: "left",
+          sortable: true,
+          value: "ArrayTaskId"
+        },
+        {
+          text: "Name",
+          align: "left",
+          sortable: true,
+          value: "JobName"
+        },
+        {
+          text: "State",
+          align: "left",
+          sortable: true,
+          value: "JobState"
+        },
+        {
+          text: "Nodes",
+          align: "left",
+          sortable: true,
+          value: "NodeNames"
+        },
+        {
+          text: "Cores",
+          align: "left",
+          sortable: true,
+          value: "NumCPUs"
+        },
+        {
+          text: "Runtime",
+          align: "left",
+          sortable: true,
+          value: "RunTime"
+        },
+        {
+          text: "Submit / Start",
+          align: "left",
+          sortable: true,
+          value: "StartTime"
+        }
+      ],
+      search: ""
+    };
+  },
+  methods: {
+    capitalize
+  }
+};
+</script>
--- src/components/Jobs.vue
+++ src/components/Jobs.vue
@@ -1,123 +1,21 @@
 <template>
   <v-container fluid>
-    <v-card>
-      <v-card-title>
-        {{jobstatus.length}} Jobs
-        <v-spacer></v-spacer>
-        <v-flex xs="12" md="6">
-          <v-text-field append-icon="search"
-                        label="Search"
-                        single-line
-                        hide-details
-                        v-model="search"></v-text-field>
-        </v-flex>
-      </v-card-title>
-      <v-data-table :headers="headers"
-                    :items="jobstatus"
-                    :search="search"
-                    hide-actions
-                    disable-initial-sort>
-        <template slot="items" slot-scope="props">
-          <td><router-link :to="`/jobs/${props.item.JobId}`">{{props.item.JobId}}</router-link></td>
-          <td><router-link :to="`/users`">{{props.item.UserName}}</router-link></td>
-          <td>{{props.item.ArrayTaskId}}</td>
-          <td>{{props.item.JobName}}</td>
-          <td>{{capitalize(props.item.JobState)}}</td>
-          <td>
-            <span v-for="node in props.item.NodeNames" :key="node">
-              <router-link :to="`/nodes`">{{node}}</router-link>
-              &nbsp;
-            </span>
-          </td>
-          <td>{{props.item.NumCPUs}}</td>
-          <td>{{props.item.RunTime}}</td>
-          <td>
-            <time :datetime="props.item.StartTime === 'Unknown' ? props.item.SubmitTime : props.item.StartTime">
-              {{(props.item.StartTime === "Unknown" ? props.item.SubmitTime : props.item.StartTime).replace("T", " ")}}
-            </time>
-          </td>
-        </template>
-      </v-data-table>
-    </v-card>
-
+    <JobList :title="`${jobstatus.length} Jobs`"
+             :items="jobstatus">
+    </JobList>
   </v-container>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
-import { capitalize } from "../utils/capitalize";
+import JobList from "@/components/JobList";
 
 export default {
-  data() {
-    return {
-      headers: [
-        {
-          text: "JobID",
-          align: "left",
-          sortable: true,
-          value: "JobId"
-        },
-        {
-          text: "User",
-          align: "left",
-          sortable: true,
-          value: "UserName"
-        },
-        {
-          text: "ArrayTaskId",
-          align: "left",
-          sortable: true,
-          value: "ArrayTaskId"
-        },
-        {
-          text: "Name",
-          align: "left",
-          sortable: true,
-          value: "JobName"
-        },
-        {
-          text: "State",
-          align: "left",
-          sortable: true,
-          value: "JobState"
-        },
-        {
-          text: "Nodes",
-          align: "left",
-          sortable: true,
-          value: "NodeNames"
-        },
-        {
-          text: "Cores",
-          align: "left",
-          sortable: true,
-          value: "NumCPUs"
-        },
-        {
-          text: "Runtime",
-          align: "left",
-          sortable: true,
-          value: "RunTime"
-        },
-        {
-          text: "Submit / Start",
-          align: "left",
-          sortable: true,
-          value: "StartTime"
-        }
-      ],
-      search: ""
-    };
+  components: {
+    JobList
   },
   computed: {
     ...mapGetters(["jobstatus"])
-  },
-  methods: {
-    capitalize
   }
 };
 </script>
-
-<style scoped>
-
-</style>
