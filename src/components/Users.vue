4e9abae61c1cf08db91a200aacf7786768285b0b
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2018-06-05 16:01:54 +0200

    Remove unused style block and method from Users.vue

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -96,21 +96,6 @@
     },
     computed: {
       ...mapGetters(["userstatus"])
-    },
-    methods: {
-      capitalize(word) {
-        switch (word.length) {
-          case 0:
-            return "";
-          case 1:
-            return word.toUpperCase();
-          default:
-            return `${word[0].toUpperCase() + word.slice(1).toLowerCase()}`;
-        }
-      }
     }
   };
 </script>
-
-<style scoped>
-</style>
