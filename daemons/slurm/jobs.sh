3596d7c0407131c0e7adba4cd53058cfb3faa69b
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2018-06-05 15:34:09 +0200

    slurm/jobs.sh: properly parse paths with spaces

--- daemons/slurm/jobs.sh
+++ daemons/slurm/jobs.sh
@@ -6,4 +6,5 @@ jobs=$(scontrol show jobs --oneliner)
 
 grep "No jobs" <<< "$jobs" &>/dev/null && { echo '[]'; exit 0; }
 
-sed -e 's/\(\S\+\)=\(\S*\)/"\1": "\2",/g' -e 's/^\(.\+\),\s*$/{\1},/' -e '1s/^/[/' -e '$s/},$/}]/' <<< "$jobs"
+perl -pe 's/(\S+)=(.*?)(?=\S+=|$)/"\1": "\2", /g'  <<< "$jobs" \
+    | sed -r -e 's/^(.+),\s*$/{\1},/' -e '1s/^/[/' -e '$s/},$/}]/'
