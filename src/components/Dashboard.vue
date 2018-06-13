37dda37dba482bd3526e84f8d7e3e36ad5310d53
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2018-06-13 10:28:26 +0200

    Link to Node and Users pages in Dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -11,7 +11,7 @@
         <v-progress-circular v-for="node in partition.Nodes" :key="node.NodeName"
                              :value="100*node.CPUAlloc/node.CPUTot"
                              :color="node.CPUAlloc == node.CPUTot ? 'light-blue' : 'green'">
-          <router-link to="/nodes">{{node.NodeName.replace(/\D/g,'')}}</router-link>
+          <router-link :to="`/node/${node.NodeName}`">{{node.NodeName.replace(/\D/g,'')}}</router-link>
         </v-progress-circular>
       </v-card-text>
     </v-card>
@@ -24,7 +24,7 @@
     <v-card>
       <v-card-title><v-btn to="/users">Users</v-btn> Overview</v-card-title>
       <v-card-text v-for="user in userstatus" :key="user.UserName">
-        {{user.UserName}}: {{user.RunningJobs.length}} running, {{user.OtherJobs.length}} pending or completed, {{user.NumCPUs}} Cores
+        <router-link :to="`/users`">{{user.UserName}}</router-link>: {{user.RunningJobs.length}} running, {{user.OtherJobs.length}} pending or completed, {{user.NumCPUs}} Cores
       </v-card-text>
     </v-card>
   </v-container>
