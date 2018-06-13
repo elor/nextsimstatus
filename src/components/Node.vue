6600db9e473fb6b7a632532dca0aa7b82c399779
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2018-06-13 10:24:59 +0200

    Show Jobs in Node pages

--- src/components/Node.vue
+++ src/components/Node.vue
@@ -2,22 +2,31 @@
   <v-container fluid>
     <v-card>
       <v-card-title>
-        <h2>Node {{NodeName}}{{Node ? `: ${Node.NodeName}` : ''}}</h2>
+        <h2>Node {{NodeName}}</h2>
       </v-card-title>
 
       <v-card-text>
-        <pre v-if="Node">{{Node}}</pre>
+        <pre v-if="NodeShort">{{ NodeShort }}</pre>
         <span v-else>Noch keine Daten empfangen</span>
       </v-card-text>
     </v-card>
+
+    <JobList v-if="Node && Jobs.length"
+             :title="`${Jobs.length} Jobs`"
+             :items="Jobs">
+    </JobList>
 
   </v-container>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
+import JobList from "@/components/JobList";
 
 export default {
+  components: {
+    JobList
+  },
   computed: {
     ...mapGetters(["nodestatus"]),
     NodeName() {
@@ -25,6 +34,18 @@ export default {
     },
     Node() {
       return this.nodestatus.filter(node => node.NodeName === this.NodeName)[0];
+    },
+    NodeShort() {
+      let node = { ...this.Node };
+
+      delete node.jobs;
+      delete node.pureJobs;
+      delete node.jobArrays;
+
+      return node;
+    },
+    Jobs() {
+      return this.Node.jobs;
     }
   }
 };
