0d75d1c14f68373f10e454405c60bba4292e2c6d
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2018-07-25 11:51:53 +0200

    Add Link to GitLab in Software

--- src/components/Software.vue
+++ src/components/Software.vue
@@ -1,21 +1,25 @@
 <template>
   <v-container fluid>
-    <v-btn color="primary" to="apps/periodyctionary">Periodyctionary</v-btn>
+    <v-layout>
+      <v-btn color="primary" to="apps/periodyctionary">Periodyctionary</v-btn>
+    </v-layout>
+    <v-layout>
+      <v-btn color="primary" href="http://mainsim.etit.tu-chemnitz.de:4485">GitLab</v-btn>
+    </v-layout>
   </v-container>
 </template>
 
 <script>
-  import { mapGetters } from "vuex";
+import { mapGetters } from "vuex";
 
-  export default {
-    data() {
-      return {
-      };
-    },
-    computed: {
-      ...mapGetters(["nodestatus", "queuestatus"])
-    }
-  };
+export default {
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapGetters(["nodestatus", "queuestatus"])
+  }
+};
 </script>
 
 <style scoped>
