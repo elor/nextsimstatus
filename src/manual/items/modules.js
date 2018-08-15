fc7d34000c15a9fd7a496fc9cf540397231711ea
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2018-08-15 08:49:24 +0200

    Gruppeneigene Modules ergänzt

--- src/manual/items/modules.js
+++ src/manual/items/modules.js
@@ -12,4 +12,22 @@ Dann kannst du die darüber geladene Software direkt nutzen.
 
 Mehr erfährst du über \`module --help\` sowie \`man module\`.
 `
+}, {
+  title: "Gruppeneigene Modules",
+  markdown: `
+Du kannst eigene Software gezielt für dich oder deine Gruppe als Module installieren.
+Dafür musst du eine Modulefile erstellen und in dem entsprechenden \`modulefiles\`-Verzeichnis ablegen.
+
+Schau dir für einen Überblick, am besten \`/beegfs-home/modules/modulefiles\` an und vergleiche es mit dem Output von \`module spider\` und \`module avail\`.
+
+Die gruppeneigenen Modules sind in den Austauschverzeichnissen der Nutzergruppen in den vorhandenen \`modules\`-Ordner zu installieren.
+Um sie nach der Installation zu nutzen, füge deiner \`.bashrc\`-Datei in deinem Home-Verzeichnis folgende Zeile hinzu:
+
+    module use /beegfs-home/share/{Gruppe}/modules/modulefiles
+
+    z.B.
+    module use /beegfs-home/share/skalmod/modules/modulefiles
+
+Auf diese Art kannst du auch nutzereigene Modules erstellen.
+`
 }];
