f0deb7b88c8cc43e40c3754416441bf859ec039e
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2018-10-11 11:30:52 +0200

    Add JobList to User Page

--- src/components/User.vue
+++ src/components/User.vue
@@ -6,24 +6,54 @@
       </v-card-title>
 
       <v-card-text>
-        <pre v-if="User">{{ User }}</pre>
+        <pre v-if="User_short">{{ User_short }}</pre>
         <span v-else>Noch aktuellen Daten verfügbar</span>
       </v-card-text>
     </v-card>
+
+    <JobList v-if="Jobs && Jobs.length"
+             :title="`${Jobs.length} Jobs (${User.JobCount.Running} Running, ${User.JobCount.Other} Other)`"
+             :items="Jobs">
+    </JobList>
+
   </v-container>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
+import JobList from "@/components/JobList";
+import { cloneDeep } from "lodash";
 
 export default {
+  components: {
+    JobList
+  },
   computed: {
     ...mapGetters(["userstatus"]),
     username() {
       return this.$route.params.id;
     },
     User() {
-      return this.userstatus.filter(user => user.UserName === this.username)[0];
+      return this.userstatus.filter(
+        user => user.UserName === this.username || user.UserID === this.username
+      )[0];
+    },
+    User_short() {
+      if (!this.User) {
+        return undefined;
+      }
+      const User = cloneDeep(this.User);
+      delete User.Jobs;
+      delete User.RunningJobs;
+      delete User.OtherJobs;
+      delete User.RunningArrays;
+      delete User.RunningPureJobs;
+      delete User.OtherArrays;
+      delete User.OtherPureJobs;
+      return User;
+    },
+    Jobs() {
+      return this.User.Jobs;
     }
   }
 };
