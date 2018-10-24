24fd5c89ccbef7e0014e60c730d59b58e186be61
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2018-10-24 13:30:00 +0200

    Add GridCard component for Grid Cards

---
+++ src/components/GridCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-flex lg3 sm6 xs12>
+    <v-card class="gridcard" ma-2>
+      <v-layout row>
+        <v-subheader>{{title}}</v-subheader>
+      </v-layout>
+      <v-card-text>
+        <slot></slot>
+      </v-card-text>
+    </v-card>
+  </v-flex>
+</template>
+
+<script>
+export default {
+  props: ["title"],
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+.gridcard {
+  min-height: 155px;
+  max-height: 170px;
+}
+</style>
