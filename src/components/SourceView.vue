0b71d23ab0a83a83e131ac2e45bdfdfc9eb49791
Erik E. Lorenz <erik@tuvero.de>
2018-10-26 09:57:00 +0200

    Add SourceView for viewing JSON sources

---
+++ src/components/SourceView.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-dialog v-model="dialog" scrollable>
+
+    <v-btn slot="activator" flat color="primary" outline>
+      <slot name="button-content">
+        <v-icon>code</v-icon>
+        View Source
+      </slot>
+    </v-btn>
+
+    <v-card @keyup.esc="close">
+      <v-toolbar dark color="primary">
+        <v-btn icon @click.native="close" dark>
+          <v-icon>close</v-icon>
+        </v-btn>
+        <v-toolbar-title>{{title}}</v-toolbar-title>
+      </v-toolbar>
+
+      <v-card-text>
+        <v-textarea auto-grow
+                    autofocus
+                    solo
+                    :value="json_value"></v-textarea>
+      </v-card-text>
+
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: ["title", "value"],
+  data() {
+    return {
+      dialog: true
+    };
+  },
+  computed: {
+    json_value() {
+      return JSON.stringify(this.value, undefined, "  ");
+    }
+  },
+  methods: {
+    close() {
+      this.dialog = false;
+    }
+  }
+};
+</script>
