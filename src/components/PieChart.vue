34d405f3adffa8a6e8db17a561372348790d08f2
Erik E. Lorenz <erik@tuvero.de>
2018-10-26 13:15:42 +0200

    Add ChartJS config to PieChart

--- src/components/PieChart.vue
+++ src/components/PieChart.vue
@@ -1,12 +1,14 @@
 <script>
 import { Pie, mixins } from "vue-chartjs";
 
+const config = {};
+
 export default {
   extends: Pie,
   mixins: [mixins.reactiveProp],
   mounted() {
     // Overwriting base render method with actual data.
-    this.renderChart(this.chartData, this.options);
+    this.renderChart(this.chartData, config);
   }
 };
 </script>
