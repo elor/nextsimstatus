5d0c20548ed6e0a3e05a80e3af3477f74b601dfa
Erik E. Lorenz <erik@tuvero.de>
2018-10-26 14:32:33 +0200

    Add Cores pie chart to Dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -2,17 +2,25 @@
   <v-container fluid>
     <v-card>
       <v-card-title><v-btn to="/nodes">Nodes</v-btn> Overview</v-card-title>
-      <v-card-text v-for="partition in partitionstatus" :key="partition.PartitionName">
-        <v-progress-circular :value="100*partition.CPUAlloc/partition.CPUTot"
-                             :color="partition.CPUAlloc == partition.CPUTot ? 'light-blue' : 'green'">
-          <b><router-link to="/nodes">{{partition.PartitionName}}</router-link></b>
-        </v-progress-circular>
-        &nbsp;
-        <v-progress-circular v-for="node in partition.Nodes" :key="node.NodeName"
-                             :value="100*node.CPUAlloc/node.CPUTot"
-                             :color="node.CPUAlloc == node.CPUTot ? 'light-blue' : 'green'">
-          <router-link :to="`/${node.NodeName}`">{{node.NodeName.replace(/\D/g,'')}}</router-link>
-        </v-progress-circular>
+      <v-card-text>
+        <v-layout row wrap>
+          <grid-card height="250" title="Core Allocation">
+            <cores-pie-chart height="100" hidelegend></cores-pie-chart>
+          </grid-card>
+
+          <grid-card height="250" v-for="partition in partitionstatus" :key="partition.PartitionName"
+                     :title="`${partition.PartitionName} Partition`">
+            <v-progress-circular slot="icon" :value="100*partition.CPUAlloc/partition.CPUTot"
+                                :color="partition.CPUAlloc == partition.CPUTot ? 'light-blue' : 'green'">
+              <b><router-link to="/nodes">{{partition.PartitionName}}</router-link></b>
+            </v-progress-circular>
+            <v-progress-circular v-for="node in partition.Nodes" :key="node.NodeName"
+                                :value="100*node.CPUAlloc/node.CPUTot"
+                                :color="node.CPUAlloc == node.CPUTot ? 'light-blue' : 'green'">
+              <router-link :to="`/${node.NodeName}`">{{node.NodeName.replace(/\D/g,'')}}</router-link>
+            </v-progress-circular>
+          </grid-card>
+        </v-layout>
       </v-card-text>
     </v-card>
 
@@ -43,8 +51,14 @@
 
 <script>
 import { mapGetters } from "vuex";
+import GridCard from "@/components/GridCard";
+import CoresPieChart from "@/components/CoresPieChart";
 
 export default {
+  components: {
+    GridCard,
+    CoresPieChart
+  },
   computed: {
     ...mapGetters(["partitionstatus", "jobstatus", "userstatus", "simpcstatus"])
   }
