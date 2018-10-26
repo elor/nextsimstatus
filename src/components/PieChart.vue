2981cf5732e3706f14b35c053e2a725ef637bfc1
Erik E. Lorenz <erik@tuvero.de>
2018-10-26 14:32:03 +0200

    extensive PieChart upgrades in data flow and configuration

--- src/components/PieChart.vue
+++ src/components/PieChart.vue
@@ -1,14 +1,47 @@
+<template>
+  <canvas ref="canvas" :height="height" :style="style"></canvas>
+</template>
+
 <script>
 import { Pie, mixins } from "vue-chartjs";
 
-const config = {};
-
 export default {
   extends: Pie,
+  props: {
+    height: { default: 200 },
+    hidelegend: Boolean
+  },
   mixins: [mixins.reactiveProp],
+  computed: {
+    config() {
+      return {
+        legend: {
+          position: "right",
+          display: !this.hidelegend
+        },
+        animation: {
+          animateRotate: false
+        }
+      };
+    },
+    style() {
+      let style = {};
+
+      if (this.inline) {
+        style.display = "inline";
+      }
+
+      return style;
+    }
+  },
+  watch: {
+    hidelegend() {
+      this.render();
+    }
+  },
   mounted() {
     // Overwriting base render method with actual data.
-    this.renderChart(this.chartData, config);
+    this.renderChart(this.chartData, this.config);
   }
 };
 </script>
