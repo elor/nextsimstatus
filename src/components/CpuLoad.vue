ff5f74d1ee3fdf8f730cf06a0682648d6ad7d659
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2018-11-07 10:55:21 +0100

    add CPULoad progress circle

---
+++ src/components/CpuLoad.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-progress-circular :value="percent"
+                      :color="color">
+                      {{Math.round(percent)}}
+  </v-progress-circular>
+</template>
+
+<script>
+export default {
+  props: {
+    percent: Number
+  },
+  computed: {
+    color() {
+      if (this.percent >= 110) {
+        return "red";
+      } else if (this.percent >= 90) {
+        return "light-blue";
+      } else {
+        return "green";
+      }
+    }
+  }
+};
+</script>
