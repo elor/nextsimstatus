c7dec31e03589216e20322b6c60c92285c7c3ae0
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2018-12-20 09:58:45 +0100

    Reorder MAIN Links; Add Links to ENAS stuff

--- src/components/Software.vue
+++ src/components/Software.vue
@@ -1,18 +1,18 @@
 <template>
   <v-container fluid>
     <v-layout>
-      <v-btn color="primary" to="apps/periodyfier">Periodyfier</v-btn>
+      <v-btn href="https://mainsim.etit.tu-chemnitz.de/gitlab">GitLab</v-btn>
+      <v-btn to="apps/periodyfier">Periodyfier</v-btn>
+      <v-btn href="https://www.tu-chemnitz.de/main/">MAIN-Webseite</v-btn>
     </v-layout>
     <v-layout>
-      <v-btn color="primary" href="https://mainsim.etit.tu-chemnitz.de/gitlab">GitLab</v-btn>
-    </v-layout>
-    <v-layout>
-      <v-btn color="primary" href="https://www.tu-chemnitz.de/main/">MAIN-Webseite</v-btn>
-    </v-layout>
-    <v-layout>
-      <v-btn href="https://tuc.cloud">TUC-Cloud (Cloudspeicher)</v-btn>
+      <v-btn href="https://tuc.cloud">TUC-Cloud</v-btn>
       <v-btn href="https://terminplaner4.dfn.de/">DFN Terminplaner</v-btn>
       <v-btn href="http://www.conf.dfn.de/">DFN Konferenztelefonie</v-btn>
+    </v-layout>
+    <v-layout>
+      <v-btn href="https://owncloud.fraunhofer.de">Fraunhofer-Cloud</v-btn>
+      <v-btn href="http://intranet.intern.enas.fraunhofer.de">ENAS-Intranet</v-btn>
     </v-layout>
   </v-container>
 </template>
