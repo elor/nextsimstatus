55f9b9b3b4461ef3acf6a5fc0711fbacecb2c06b
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2019-01-28 13:29:21 +0100

    autolint CPULoad

--- src/components/CpuLoad.vue
+++ src/components/CpuLoad.vue
@@ -1,33 +1,32 @@
 <template>
-  <v-progress-circular :value="percent"
-                      :color="color">
-                      {{value}}
+  <v-progress-circular :value="percent" :color="color">
+    {{value}}
   </v-progress-circular>
 </template>
 
 <script>
-export default {
-  props: {
-    load: Number,
-    cores: Number,
-    precise: Boolean
-  },
-  computed: {
-    color() {
-      if (this.percent >= 110) {
-        return "red";
-      } else if (this.percent >= 90) {
-        return "light-blue";
-      } else {
-        return "green";
+  export default {
+    props: {
+      load: Number,
+      cores: Number,
+      precise: Boolean
+    },
+    computed: {
+      color() {
+        if (this.percent >= 110) {
+          return "red";
+        } else if (this.percent >= 90) {
+          return "light-blue";
+        } else {
+          return "green";
+        }
+      },
+      percent() {
+        return 100 * this.load / this.cores;
+      },
+      value() {
+        return this.precise ? this.load : Math.round(this.load);
       }
-    },
-    percent(){
-      return 100 * this.load / this.cores;
-    },
-    value(){
-      return this.precise ? this.load : Math.round(this.load);
     }
-  }
-};
+  };
 </script>
