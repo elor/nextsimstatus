38bda196dc7cbc191f1f601bab0fef9bac70dfc5
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2019-01-28 11:39:14 +0100

    Combine Dashboard partitions into one card

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,44 +1,58 @@
 <template>
   <v-container fluid>
     <v-card>
-      <v-card-title><v-btn to="/nodes">Nodes</v-btn> Overview</v-card-title>
+      <v-card-title>
+        <v-btn to="/nodes">Nodes</v-btn>Overview
+      </v-card-title>
       <v-card-text>
         <v-layout row wrap>
           <grid-card height="250" title="Core Allocation">
             <cores-pie-chart height="180" hidelegend></cores-pie-chart>
           </grid-card>
 
-          <grid-card height="250" v-for="partition in partitionstatus" :key="partition.PartitionName"
-                     :title="`${partition.PartitionName} Partition`">
-            <v-progress-circular slot="icon" :value="100*partition.CPUAlloc/partition.CPUTot"
-                                :color="partition.CPUAlloc == partition.CPUTot ? 'light-blue' : 'green'">
-              <b><router-link to="/nodes">{{partition.PartitionName}}</router-link></b>
-            </v-progress-circular>
-            <v-progress-circular v-for="node in partition.Nodes" :key="node.NodeName"
-                                :value="100*node.CPUAlloc/node.CPUTot"
-                                :color="node.CPUAlloc == node.CPUTot ? 'light-blue' : 'green'">
-              <router-link :to="`/${node.NodeName}`">{{node.NodeName.replace(/\D/g,'')}}</router-link>
-            </v-progress-circular>
+          <grid-card title="Partitions">
+            <div class="mb-2" v-for="partition in partitionstatus" :key="partition.PartitionName">
+              <v-progress-circular
+                slot="icon"
+                :value="100*partition.CPUAlloc/partition.CPUTot"
+                :color="partition.CPUAlloc == partition.CPUTot ? 'light-blue' : 'green'"
+              >
+                <b>
+                  <router-link to="/nodes">{{partition.PartitionName}}</router-link>
+                </b>
+              </v-progress-circular>
+              <v-progress-circular
+                v-for="node in partition.Nodes"
+                :key="node.NodeName"
+                :value="100*node.CPUAlloc/node.CPUTot"
+                :color="node.CPUAlloc == node.CPUTot ? 'light-blue' : 'green'"
+              >
+                <router-link :to="`/${node.NodeName}`">{{node.NodeName.replace(/\D/g,'')}}</router-link>
+              </v-progress-circular>
+            </div>
           </grid-card>
         </v-layout>
       </v-card-text>
     </v-card>
 
     <v-card>
-      <v-card-title><v-btn to="/jobs">Jobs</v-btn> Overview</v-card-title>
+      <v-card-title>
+        <v-btn to="/jobs">Jobs</v-btn>Overview
+      </v-card-title>
       <v-card-text>{{jobstatus.length}} Jobs</v-card-text>
     </v-card>
 
     <v-card>
-      <v-card-title><v-btn to="/users">Users</v-btn> Overview</v-card-title>
+      <v-card-title>
+        <v-btn to="/users">Users</v-btn>Overview
+      </v-card-title>
       <v-card-text>
         <p class="xs-6" v-for="user in users_sorted" :key="user.UserName">
           <router-link :to="`/users/${user.UserName}`">
-          <v-chip class="ma-0" label small :style="{'background-color':user.color}"></v-chip>
+            <v-chip class="ma-0" label small :style="{'background-color':user.color}"></v-chip>
           </router-link>
-          <router-link :to="`/users/${user.UserName}`">
-            {{user.UserName}}
-          </router-link>:
+          <router-link :to="`/users/${user.UserName}`">{{user.UserName}}</router-link>
+          :
           {{user.JobCount.Running}} running,
           {{user.JobCount.Pending}} pending,
           {{user.JobCount.Completed}} completed,
@@ -49,11 +63,16 @@
     </v-card>
 
     <v-card>
-      <v-card-title><v-btn to="/simpcs">SimPCs</v-btn> Overview</v-card-title>
+      <v-card-title>
+        <v-btn to="/simpcs">SimPCs</v-btn>Overview
+      </v-card-title>
       <v-card-text>
-        <v-progress-circular v-for="pc in simpcstatus" :key="pc.hsotname"
-                              :value="pc.load_1min !== undefined ? (10 + 90 * pc.load_1min / 5.0) : 0.0"
-                              :color="pc.load_1min > 5.0 ? 'red' : 'green'">
+        <v-progress-circular
+          v-for="pc in simpcstatus"
+          :key="pc.hsotname"
+          :value="pc.load_1min !== undefined ? (10 + 90 * pc.load_1min / 5.0) : 0.0"
+          :color="pc.load_1min > 5.0 ? 'red' : 'green'"
+        >
           <router-link :to="`/simpc${pc.number}`">{{pc.number}}</router-link>
         </v-progress-circular>
       </v-card-text>
@@ -79,13 +98,15 @@ export default {
       "simpcstatus"
     ]),
     users_sorted() {
-      return this.userstatus.slice().sort(
-        (a, b) =>
-          b.NumCPUs - a.NumCPUs ||
-          b.JobCount.Running - a.JobCount.Running ||
-          b.JobCount.Pending - a.JobCount.Pending ||
-          a.UserName.localeCompare(b.UserName)
-      );
+      return this.userstatus
+        .slice()
+        .sort(
+          (a, b) =>
+            b.NumCPUs - a.NumCPUs ||
+            b.JobCount.Running - a.JobCount.Running ||
+            b.JobCount.Pending - a.JobCount.Pending ||
+            a.UserName.localeCompare(b.UserName)
+        );
     }
   }
 };
