eaee3477581d2a738a1ce79b10766ce72aa23dd8
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2019-01-28 15:47:26 +0100

    Add Rack and RackTemperature components

---
+++ src/components/Rack.vue
@@ -0,0 +1,28 @@
+<template>
+  <p>
+    {{rack.name}}
+    <br>
+    <rack-temperature :values="rack.temperatures.supply_water" title="supply_water"></rack-temperature>
+    <rack-temperature :values="rack.temperatures.supply_air" title="supply_air"></rack-temperature>
+    <rack-temperature :values="rack.temperatures.return_air" title="return_air"></rack-temperature>
+    <rack-temperature :values="rack.temperatures.return_water" title="return_water"></rack-temperature>
+    °C
+
+    {{rack.cooling_capacity}} kW
+    @
+    {{rack.water_flow}} m³/h
+  </p>
+</template>
+
+<script>
+  import RackTemperature from '@/components/RackTemperature'
+
+  export default {
+    components: {
+      RackTemperature
+    },
+    props: {
+      rack: Object
+    }
+  };
+</script>
---
+++ src/components/RackTemperature.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-progress-circular :value="percent" :color="color">
+    {{text}}
+  </v-progress-circular>
+</template>
+
+<script>
+  export default {
+    props: {
+      values: Object
+    },
+    computed: {
+      color() {
+        if (this.value >= this.high) {
+          return "red";
+        } else if (this.value <= this.low) {
+          return "light-blue";
+        } else {
+          return "green";
+        }
+      },
+      value() {
+        return this.values.val
+      },
+      min() {
+        return this.values.min
+      },
+      max() {
+        return this.values.max
+      },
+      percent() {
+        return 100 * (this.value - this.min) / (this.max - this.min);
+      },
+      text() {
+        return Math.round(this.value)
+      }
+    }
+  };
+</script>
