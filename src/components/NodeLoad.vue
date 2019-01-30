63c46f3ace61d5a8650918dfae553cc875a1c9a9
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2019-01-30 18:38:42 +0100

    Split NodeLoad.vue from Dashboard.vue

---
+++ src/components/NodeLoad.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-progress-circular :value="percent" :color="color">
+    <router-link :to="`/${name}`">{{number}}</router-link>
+  </v-progress-circular>
+</template>
+
+<script>
+import nodecolor from '../utils/nodecolor'
+
+export default {
+  props: {
+    node: Object
+  },
+  computed: {
+    percent () {
+      return 100 * this.node.CPUAlloc / this.node.CPUTot
+    },
+    color () {
+      return nodecolor(this.node)
+    },
+    name () {
+      return this.node.NodeName
+    },
+    number () {
+      return Number(this.name.replace(/\D/g, ''))
+    }
+  }
+}
+</script>
