57935dfa4d7bd0c34fb83b0ff30e82b350c35ecd
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2019-02-05 16:45:05 +0100

    increase NodeLoad red-limit to CPUTot*1.1 (was CPULoad*1.1, which was way too early)

--- src/utils/nodecolor.js
+++ src/utils/nodecolor.js
@@ -1,6 +1,6 @@
 export default function nodecolor (node) {
   switch (true) {
-    case node.CPUAlloc && node.CPULoad > node.CPUAlloc * 1.2:
+    case node.CPUAlloc && node.CPULoad > node.CPUTot * 1.1:
       return 'red'
     case node.CPUAlloc === node.CPUTot:
       return 'light-blue'
