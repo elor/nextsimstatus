0df71221f383d5f2fda37b5936ce320a362905e7
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2019-02-06 09:07:11 +0100

    Remove resolved TODOs from the Manual

--- src/manual/items/todo.js
+++ src/manual/items/todo.js
@@ -1,15 +1,12 @@
 export default [{
   title: 'TODO',
   markdown: `
-* First Steps
 * SLURM-Befehle
 * SBATCH-Dateien + Scratch
-* SSH-keygen
 * Remote Login SSH / VPN
 * Netzlaufwerke
 * RDP-Knoten
 * Gruppenchats
-* SimPCs
 * Terminal-Ressourcen (First Steps)
 `
 }]
