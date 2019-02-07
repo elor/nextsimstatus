fc4027424197cf5944c69ce38c334498eb4f54ee
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2019-02-07 16:07:01 +0100

    Change auth paths to /auth/... and add token routes

--- daemons/mainsimweb/auth/auth-server.js
+++ daemons/mainsimweb/auth/auth-server.js
@@ -1,20 +1,32 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-
+const jwt = require('./jwt')
 const ldap = require('./ldap')
 
-const app = express()
-app.use(bodyParser.urlencoded({ extended: true }))
+const PORT = 8000
 
-app.post('/login', (request, response) => {
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.post('/auth/login', (request, response) => {
   ldap.login(request.body.username, request.body.password)
-    .then(result => response.send(JSON.stringify(result, undefined, 2)))
-    .catch(error => { console.error(error); response.send('ERROR') })
+    .then(ldapMatch => jwt.sign(ldapMatch))
+    .then(token => {
+      response.setHeader('Content-Type', 'application/json')
+      response.send(JSON.stringify({ token, decoded: jwt.decode(token) }, undefined, 2))
+    })
+    .catch(error => {
+      console.error(error)
+      response.status(401)
+      response.send('Login error')
+    })
 })
 
-app.get('/login', (req, res) => {
+app.get('/auth/login', (req, res) => {
   res.send(`
-<form method="post" action="/login">
+<form method="post" action="/auth/login">
 Username: <input type="text" name="username" placeholder=""/><br>
 Password: <input type="password" name="password" placeholder=""/><br>
 <input type="submit" value="Login" />
@@ -22,6 +34,42 @@ Password: <input type="password" name="password" placeholder=""/><br>
 `)
 })
 
-app.listen(8000)
+app.post('/auth/token/check', (request, response) => {
+  const token = request.body.token
 
-console.log('serving')
+  jwt.verify(token)
+    .then(decoded => {
+      response.setHeader('Content-Type', 'application/json')
+      response.send(JSON.stringify({
+        valid: true,
+        decoded,
+        shouldRenew: jwt.shouldRenew(token)
+      }))
+    })
+    .catch(err => {
+      console.error(err)
+
+      response.status(401)
+      response.send('Token verification failed')
+    })
+})
+
+app.post('/auth/token/renew', (request, response) => {
+  const token = request.body.token
+
+  jwt.renew(token)
+    .then(token => {
+      response.setHeader('Content-Type', 'application/json')
+      response.send(JSON.stringify({ token, decoded: jwt.decode(token) }, undefined, 2))
+    })
+    .catch(err => {
+      console.error(err)
+
+      response.status(401)
+      response.send('Token verification failed')
+    })
+})
+
+app.listen(PORT)
+
+console.log(`serving on port ${PORT}`)
