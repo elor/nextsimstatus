fa7180fb5d16e28ba1f6a47ddf296da358243796
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2019-03-27 09:28:55 +0100

    Add mensa daemon to mainsimweb

---
+++ daemons/mainsimweb/mensa/mensa.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+const { toJson } = require('xml2json')
+const axios = require('axios')
+
+const BASE_URL = 'https://www.swcz.de/bilderspeiseplan/feed.php'
+
+module.exports = {
+  URLS: {
+    RH70_MENSA: `${BASE_URL}?plan=7`,
+    RH70_CAFETERIA: `${BASE_URL}?plan=1479835489`
+  },
+  async getRawPlan (URL) {
+    const { data } = await axios.get(URL)
+    return toJson(data)
+  }
+}
---
+++ daemons/mainsimweb/mensa/server.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const mensa = require('./mensa')
+
+const PORT = 8000
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.use(cors())
+
+app.get('/', async (request, response) => {
+  try {
+    const oben = await mensa.getRawPlan(mensa.URLS.RH70_MENSA)
+    const unten = await mensa.getRawPlan(mensa.URLS.RH70_CAFETERIA)
+
+    const data = { mensa: oben, cafeteria: unten }
+
+    response.setHeader('Content-Type', 'application/json')
+    response.send(JSON.stringify(data, undefined, 2))
+  } catch (error) {
+    console.error(error)
+    response.status(401)
+    response.send('Login error')
+  }
+})
+
+app.listen(PORT)
+
+console.log(`serving on port ${PORT}`)
