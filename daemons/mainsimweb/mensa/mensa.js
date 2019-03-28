fffa924fb314bf588638216f0d082aa011dbc0bf
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2019-03-28 10:52:32 +0100

    I mixed up mensa and cafeteria

--- daemons/mainsimweb/mensa/mensa.js
+++ daemons/mainsimweb/mensa/mensa.js
@@ -7,8 +7,8 @@ const BASE_URL = 'https://www.swcz.de/bilderspeiseplan/feed.php'
 
 module.exports = {
   URLS: {
-    RH70_MENSA: `${BASE_URL}?plan=7`,
-    RH70_CAFETERIA: `${BASE_URL}?plan=1479835489`
+    RH70_MENSA: `${BASE_URL}?plan=1479835489`,
+    RH70_CAFETERIA: `${BASE_URL}?plan=7`
   },
   async getRawPlan (URL) {
     const { data } = await axios.get(URL)
