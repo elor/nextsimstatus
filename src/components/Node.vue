4352930a1a36f05d79a23d7c1e9e8d876da48357
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2019-03-28 14:34:31 +0100

    Add Gres info to Node page

--- src/components/Node.vue
+++ src/components/Node.vue
@@ -18,8 +18,15 @@
               <v-progress-circular :value="MemPercent" :color="MemPercent > 90 ? 'red' : 'light-blue'">
                 {{MemPercent}}
               </v-progress-circular>
+              <v-progress-circular v-for="gres in Gres" :key="gres.ressource" :value="gres.percent" color='light-blue'>
+                {{gres.jobs}}/{{gres.total}}
+              </v-progress-circular>
             </template>
-            {{Node.CPUAlloc}} / {{Node.CPUTot}} allocated <span v-if="Node.CPUErr != 0">({{Node.CPUErr}} Err)</span><br>
+            {{Node.CPUAlloc}} / {{Node.CPUTot}} allocated <span v-if="Node.CPUErr != 0">({{Node.CPUErr}} Err)</span>
+            <span v-for="gres in Gres" :key="gres.ressource">
+              , {{gres.ressource}}: {{gres.jobs}}/{{gres.total}}
+            </span>
+            <br>
             <a href="https://en.wikipedia.org/wiki/Load_(computing)">Load</a>: {{Node.CPULoad}} ({{CPULoadPercent}}%)<br>
             RAM: {{Math.round((Node.RealMemory - Node.FreeMem)/1000)}}/{{Node.RealMemory / 1000}} GB ({{MemPercent}}%)
           </grid-card>
@@ -60,6 +67,12 @@ import JobList from '@/components/JobList'
 import GridCard from '@/components/GridCard'
 import SourceView from '@/components/SourceView'
 import CpuLoad from '@/components/CpuLoad'
+import { uniq, concat } from 'lodash'
+
+function deGres (Gres) {
+  const [type, count] = Gres.split(':')
+  return { [type]: Number(count) }
+}
 
 export default {
   components: {
@@ -75,6 +88,37 @@ export default {
     },
     Node () {
       return this.nodestatus.filter(node => node.NodeName === this.NodeName)[0]
+    },
+    Gres () {
+      if (!this.Node.Gres || this.Node.Gres === '(null)') {
+        return undefined
+      }
+
+      const nodeGres = deGres(this.Node.Gres)
+      const jobGres = this.Jobs
+        .filter(job=>job.Gres)
+        .map(job => job.Gres)
+        .map(deGres)
+        .reduce((a,b) => {
+          for (let key in b) {
+            a[key] = b[key] + (a[key] || 0)
+          }
+          return a
+        }, {})
+
+      const allKeys = uniq(concat(Object.keys(nodeGres), Object.keys(jobGres)))
+
+      if (!allKeys) {
+        return undefined
+      }
+
+      return allKeys.map(key => ({
+        jobs: jobGres[key] || 0,
+        total: nodeGres[key] || 0,
+        ressource: key})
+      ).map(gres => ({...gres,
+        percent: Math.round(100*gres.jobs / (gres.total || 1))})
+      )
     },
     MemPercent () {
       return 100 - Math.round((100 * this.Node.FreeMem) / this.Node.RealMemory)
