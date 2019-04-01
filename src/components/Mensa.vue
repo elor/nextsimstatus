b167a4defad99f9f0ad7729a3cd4eacda3b5c610
Erik E. Lorenz <erik@tuvero.de>
2019-04-01 17:21:54 +0200

    add proper headline for mensa view

--- src/components/Mensa.vue
+++ src/components/Mensa.vue
@@ -4,30 +4,28 @@
       <v-flex md6 xs12 v-for="[type, menu] in Object.entries({mensa, cafeteria})" :key="type">
         <v-card class="ma-2">
           <v-card-title class="headline">{{capitalize(type)}} ({{formatMenuDate(menu.datum)}})</v-card-title>
-          <v-card-text>
-            <v-card v-for="essen in menu.essen" :key="essen.id">
-              <v-layout>
-                <v-flex xs4>
-                  <v-img :src="essen.img_big" height="150px" contain></v-img>
-                </v-flex>
-                <v-flex xs8>
-                  <v-card-title primary-title>
-                    <div>
-                      <div class="headline">{{essen.kategorie}}</div>
-                      <div>{{essen.deutsch}}</div>
-                      <div v-if="essen.pr">
-                        Preis:
-                        <span
-                          v-for="[id, preis] in Object.entries(preise(essen.pr))"
-                          :key="id"
-                        >{{preis}}€</span>
-                      </div>
-                    </div>
-                  </v-card-title>
-                </v-flex>
-              </v-layout>
-            </v-card>
-          </v-card-text>
+        </v-card>
+        <v-card v-for="essen in menu.essen" :key="essen.id" class="mx-2">
+          <v-layout>
+            <v-flex xs4>
+              <v-img :src="essen.img_big" height="150px" contain></v-img>
+            </v-flex>
+            <v-flex xs8>
+              <v-card-title primary-title>
+                <div>
+                  <div class="headline">{{essen.kategorie}}</div>
+                  <div>{{essen.deutsch}}</div>
+                  <div v-if="essen.pr">
+                    Preis:
+                    <span v-for="[id, preis] in Object.entries(preise(essen.pr))" :key="id">
+                      {{preis}}€
+                      &nbsp;
+                    </span>
+                  </div>
+                </div>
+              </v-card-title>
+            </v-flex>
+          </v-layout>
         </v-card>
       </v-flex>
     </v-layout>
