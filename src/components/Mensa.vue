d1d2aad8515c24f6dbba0d0eb28b422cb59176e9
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2019-04-02 10:23:58 +0200

    use preprocessed mensa data in Mensa.vue - No need to repeat the functions here

--- src/components/Mensa.vue
+++ src/components/Mensa.vue
@@ -3,7 +3,7 @@
     <v-layout row wrap v-if="mensa || cafeteria">
       <v-flex md6 xs12 v-for="[type, menu] in Object.entries({mensa, cafeteria})" :key="type">
         <v-card class="ma-2">
-          <v-card-title class="headline">{{capitalize(type)}} ({{formatMenuDate(menu.datum)}})</v-card-title>
+          <v-card-title class="headline">{{menu.name}} - {{menu.datum_str}}</v-card-title>
         </v-card>
         <v-card v-for="essen in menu.essen" :key="essen.id" class="mx-2">
           <v-layout>
@@ -14,14 +14,14 @@
               <v-card-title primary-title>
                 <div>
                   <div class="headline">{{essen.kategorie}}</div>
-                  <div>{{stripAllergens(essen.deutsch)}}</div>
+                  <div>{{essen.deutsch_short}}</div>
                   <div v-if="essen.pr">
                     Preis:
                     <span
-                      v-for="[id, preis] in Object.entries(preise(essen.pr))"
+                      v-for="[id, preis] in Object.entries(essen.preise)"
                       :key="id"
                       class="mx-2"
-                    >{{preis}}€</span>
+                    >{{preis}}</span>
                   </div>
                 </div>
               </v-card-title>
@@ -34,53 +34,32 @@
 </template>
 
 <script>
-import axios from "axios";
+import axios from 'axios'
 
-const URL = "https://mainsimweb.etit.tu-chemnitz.de/mensa";
+const URL = 'https://mainsimweb.etit.tu-chemnitz.de/mensa'
 
 export default {
-  data() {
+  data () {
     return {
       mensa: undefined,
       cafeteria: undefined
-    };
+    }
   },
   methods: {
-    capitalize(text) {
-      return `${text.charAt(0).toUpperCase()}${text.substr(1).toLowerCase()}`;
-    },
-    formatMenuDate(datum) {
-      return `${datum.tag}.${datum.monat}.${datum.jahr}`;
-    },
-    stripAllergens(essen) {
-      return essen
-        .replace(/\(\d+(\s*,\s*\d*)*\)?/g, "")
-        .replace(/\s*,\s*/g, ", ")
-        .replace(/\s+/g, " ");
-    },
-    preise(pr) {
-      if (!pr) {
-        return [];
-      }
-      if (pr.$t) {
-        return [pr.$t];
-      }
-      return pr.map(preis => preis.$t);
-    },
-    fetch() {
+    fetch () {
       axios
         .get(URL)
         .then(response => {
-          this.mensa = response.data.mensa;
-          this.cafeteria = response.data.cafeteria;
+          this.mensa = response.data.mensa
+          this.cafeteria = response.data.cafeteria
         })
         .catch(error => {
-          console.error(error);
-        });
+          console.error(error)
+        })
     }
   },
-  mounted() {
-    this.fetch();
+  mounted () {
+    this.fetch()
   }
-};
+}
 </script>
