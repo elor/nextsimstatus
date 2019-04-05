e79520ecc1560419d18f655d0dcfd16967e29f44
Erik E. Lorenz <erik@tuvero.de>
2019-04-05 13:54:37 +0200

    add drainstates to nodeState

--- src/utils/nodeStates.js
+++ src/utils/nodeStates.js
@@ -1,2 +1,3 @@
 export const warnstates = ['DRAIN', 'MAINT', 'DOWN', 'POWER_UP', 'POWER_DOWN', '@']
 export const failstates = ['NoResp', 'FAILING', 'ERROR', 'FAIL', '*']
+export const drainstates = ['DRAIN']
