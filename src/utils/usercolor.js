486d9589a47693da688240d29cb77edbf1373008
Erik E. Lorenz <erik@tuvero.de>
2019-04-05 14:21:44 +0200

    add usercolors for 'fail' and 'drain'

--- src/utils/usercolor.js
+++ src/utils/usercolor.js
@@ -3,13 +3,16 @@ import md5 from './md5'
 const colors = {
   blue: '#03a9f4',
   green: '#4caf50',
-  red: '#f44336'
+  red: '#f44336',
+  plum: '#c37cfc'
 }
 
 const mapping = {
   free: colors.green,
   error: colors.red,
   err: colors.red,
+  fail: colors.red,
+  drain: colors.plum,
   alloc: colors.blue
 }
 
