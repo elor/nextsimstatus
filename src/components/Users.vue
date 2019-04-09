10b344636c5114567262d0c2f6641b2d9637309f
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2019-04-09 16:45:45 +0200

    User UserChip in Users.vue

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -5,46 +5,54 @@
         <v-layout wrap>
           <v-spacer></v-spacer>
           <v-flex xs="12" md="6">
-            <v-text-field append-icon="search"
-                          label="Search"
-                          single-line
-                          hide-details
-                          v-model="search"></v-text-field>
+            <v-text-field
+              append-icon="search"
+              label="Search"
+              single-line
+              hide-details
+              v-model="search"
+            ></v-text-field>
           </v-flex>
         </v-layout>
       </v-card-title>
-      <v-data-table :headers="headers"
-                    :items="userstatus"
-                    :search="search"
-                    hide-actions>
+      <v-data-table :headers="headers" :items="userstatus" :search="search" hide-actions>
         <template slot="items" slot-scope="props">
-          <td><router-link :to="`/users/${props.item.UserName}`">{{props.item.UserName}}</router-link></td>
+          <td>
+            <user-chip :login="props.item.UserName"/>
+          </td>
           <td>
             <span v-for="pc in props.item.PCs" :key="pc.number">
-              <router-link :class="{'grey--text':pc.inactive}" :to="`/simpc${pc.number}`">{{pc.hostname}}</router-link>
-              &nbsp;
+              <router-link
+                :class="{'grey--text':pc.inactive}"
+                :to="`/simpc${pc.number}`"
+              >{{pc.hostname}}</router-link>&nbsp;
             </span>
           </td>
-          <td><router-link :to="`/users/${props.item.UserName}`">{{props.item.JobCount.Running}}</router-link></td>
+          <td>
+            <router-link :to="`/users/${props.item.UserName}`">{{props.item.JobCount.Running}}</router-link>
+          </td>
           <td>{{props.item.NumCPUs}}</td>
           <td>
             <node-load v-for="node in nodes(props.item)" :key="node.NodeName" :node="node"></node-load>
           </td>
-          <td><router-link :to="`/users/${props.item.UserName}`">{{props.item.JobCount.Other}}</router-link></td>
+          <td>
+            <router-link :to="`/users/${props.item.UserName}`">{{props.item.JobCount.Other}}</router-link>
+          </td>
         </template>
       </v-data-table>
     </v-card>
-
   </v-container>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 import NodeLoad from '@/components/NodeLoad'
+import UserChip from '@/components/UserChip'
 
 export default {
   components: {
-    NodeLoad
+    NodeLoad,
+    UserChip
   },
   data () {
     return {
