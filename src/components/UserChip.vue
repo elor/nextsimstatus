8f0aa9d80b8e24ac71ee418c6c997f09fc36cf03
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2019-04-09 09:37:46 +0200

    add UserChip.vue to convey a single user

---
+++ src/components/UserChip.vue
@@ -0,0 +1,31 @@
+<template>
+<v-chip :class="{'grey--text': inactive}" :to="`/users/${login}`" :color="background_color">
+    <v-avatar size="16" :color="color">{{initials}}</v-avatar>
+    {{login}}
+</v-chip>
+</template>
+
+<script>
+import usercolor from '../utils/usercolor'
+
+export default {
+  props: {
+    login: String,
+    inactive: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    color () {
+      return usercolor(this.login, this.inactive)
+    },
+    background_color () {
+      return usercolor(this.login, true)
+    },
+    initials () {
+      return this.login.replace(/[^a-z]/g, '').substr(0, 2).toUpperCase()
+    }
+  }
+}
+</script>
