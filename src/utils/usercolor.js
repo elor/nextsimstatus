dfc633352641237bb20e75dad7ff14a13c257289
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2019-04-09 09:37:38 +0200

    add `muted` argument to usercolor

--- src/utils/usercolor.js
+++ src/utils/usercolor.js
@@ -1,4 +1,5 @@
 import md5 from './md5'
+import { range, zipWith, sum } from 'lodash'
 
 const colors = {
   blue: '#03a9f4',
@@ -16,11 +17,36 @@ const mapping = {
   alloc: colors.blue
 }
 
-export default function usercolor (name) {
+const muteColor = 'ffffff'
+
+function mix (...hexColors) {
+  const colors = hexColors.map(stringToColor)
+  const mixedColor = zipWith(...colors,
+    (...components) => Math.floor(sum(components) / components.length)
+  )
+  return colorToString(mixedColor)
+}
+
+function stringToColor (colorString) {
+  return range(0, 6, 2)
+    .map(start => colorString.substr(start, 2))
+    .map(twoHexDigits => parseInt(twoHexDigits, 16))
+}
+
+function colorToString (colorArray) {
+  return colorArray.map(component => component.toString(16)).join('')
+}
+
+export default function usercolor (name, muted = false) {
   if (mapping[name.toLowerCase()]) {
     return mapping[name.toLowerCase()]
   }
 
-  const hash = md5(name + md5(name)).substr(19, 6)
-  return `#${hash}`
+  let hexColor = md5(name + md5(name)).substr(19, 6)
+
+  if (muted) {
+    hexColor = mix(hexColor, muteColor)
+  }
+
+  return `#${hexColor}`
 }
