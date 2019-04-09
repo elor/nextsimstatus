bb582a8579c5486a9dc0315348d8954a3200f859
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2019-04-09 16:18:20 +0200

    add JobChip

---
+++ src/components/JobChip.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-chip
+    small
+    :color="color"
+    :disabled="disabled"
+    :dark="isDark"
+    @click="$router.push(`/job/${job.JobId}`)"
+  >{{job.JobId}}</v-chip>
+</template>
+
+<script>
+import usercolor from '../utils/usercolor'
+import { isDark } from '../utils/color'
+
+export default {
+  props: {
+    job: {
+      type: Object,
+      default () {
+        return {
+          JobId: 0,
+          UserName: 'error'
+        }
+      }
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    color () {
+      return usercolor(this.job.UserName)
+    },
+    isDark () {
+      return isDark(this.color)
+    }
+  }
+}
+</script>
