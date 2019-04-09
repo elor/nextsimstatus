1f61b92b5d243a6b4f1c51d7afb8c04521245d5a
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2019-04-09 16:04:18 +0200

    show JobList users as UserChip

--- src/components/JobList.vue
+++ src/components/JobList.vue
@@ -4,50 +4,53 @@
       {{title}}
       <v-spacer></v-spacer>
       <v-flex xs="12" md="6">
-        <v-text-field append-icon="search"
-                      label="Search"
-                      single-line
-                      hide-details
-                      v-model="search"></v-text-field>
+        <v-text-field append-icon="search" label="Search" single-line hide-details v-model="search"></v-text-field>
       </v-flex>
     </v-card-title>
-    <v-data-table :headers="headers"
-                  :items="items"
-                  :search="search"
-                  :rows-per-page-items="[15,50,100, {text:'All', value:-1}]"
-                  disable-initial-sort>
+    <v-data-table
+      :headers="headers"
+      :items="items"
+      :search="search"
+      :rows-per-page-items="[15,50,100, {text:'All', value:-1}]"
+      disable-initial-sort
+    >
       <template slot="items" slot-scope="props">
-        <td><router-link :to="`/jobs/${props.item.JobId}`">{{props.item.JobId}}</router-link></td>
-        <td><router-link :to="`/users/${props.item.UserName}`">{{props.item.UserName}}</router-link></td>
+        <td>
+          <router-link :to="`/jobs/${props.item.JobId}`">{{props.item.JobId}}</router-link>
+        </td>
+        <td>
+          <user-chip :login="props.item.UserName"/>
+        </td>
         <td>{{props.item.ArrayTaskId}}</td>
         <td>{{props.item.JobName}}</td>
         <td>{{capitalize(props.item.JobState)}}</td>
         <td>
           <span v-for="node in props.item.NodeNames" :key="node">
-            <router-link :to="`/${node}`">{{node}}</router-link>
-            &nbsp;
+            <router-link :to="`/${node}`">{{node}}</router-link>&nbsp;
           </span>
         </td>
         <td>{{props.item.NumCPUs}}</td>
         <td>{{props.item.RunTime}}</td>
         <td>
-          <time :datetime="props.item.StartTime === 'Unknown' ? props.item.SubmitTime : props.item.StartTime">
-            {{(props.item.StartTime === "Unknown" ? props.item.SubmitTime : props.item.StartTime).replace("T", " ")}}
-          </time>
+          <time
+            :datetime="props.item.StartTime === 'Unknown' ? props.item.SubmitTime : props.item.StartTime"
+          >{{(props.item.StartTime === "Unknown" ? props.item.SubmitTime : props.item.StartTime).replace("T", " ")}}</time>
         </td>
       </template>
-      <template slot="no-data">
-        There are no jobs here.
-      </template>
+      <template slot="no-data">There are no jobs here.</template>
     </v-data-table>
   </v-card>
 </template>
 
 <script>
 import { capitalize } from '../utils/capitalize'
+import UserChip from '@/components/UserChip'
 
 export default {
   props: ['items', 'title'],
+  components: {
+    UserChip
+  },
   data () {
     return {
       headers: [
