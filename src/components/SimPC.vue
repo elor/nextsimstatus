e05c421a8db9fc8268c057e6c11940d53c4e4dcb
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2019-04-11 15:51:36 +0200

    Add a formatted SimPC page

--- src/components/SimPC.vue
+++ src/components/SimPC.vue
@@ -8,7 +8,62 @@
       </v-card-title>
 
       <v-card-text>
-        <pre v-if="SimPC">{{ SimPC }}</pre>
+        <v-layout v-if="SimPC" row wrap>
+          <grid-card title="CPU/RAM">
+            <template slot="icon">
+              <cpu-load precise :load="SimPC.load_1min" :cores="SimPC.cores"/>
+              <cpu-load precise :load="SimPC.load_5min" :cores="SimPC.cores"/>
+              <cpu-load precise :load="SimPC.load_15min" :cores="SimPC.cores"/>
+            </template>
+            <a href="https://en.wikipedia.org/wiki/Load_(computing)">Load</a>
+            1min:
+            {{SimPC.load_1min}} ({{percent_1min}}%)
+            <br>
+            <a href="https://en.wikipedia.org/wiki/Load_(computing)">Load</a>
+            5min:
+            {{SimPC.load_5min}} ({{percent_5min}}%)
+            <br>
+            <a href="https://en.wikipedia.org/wiki/Load_(computing)">Load</a>
+            15min:
+            {{SimPC.load_15min}} ({{percent_15min}}%)
+            <br>
+          </grid-card>
+
+          <grid-card title="Users">
+            <user-chip v-for="user in SimPC.usernames" :key="user" :login="user"/>
+          </grid-card>
+
+          <grid-card title="Devices">
+            <b>Mounts:</b>
+            <br>
+            <div v-for="mount in SimPC.mounts" :key="mount.mount">
+              <v-tooltip right>
+                <template v-slot:activator="{on}">
+                  <span v-on="on">{{mount.mount}}</span>
+                </template>
+                <span>{{mount.type}}@{{mount.source}}</span>
+              </v-tooltip>
+            </div>
+            <b>{{SimPC.printers ? SimPC.printers.length : 'Keine'}} Drucker</b>
+          </grid-card>
+
+          <grid-card :title="`Ubuntu ${SimPC.release}`">
+            <v-icon slot="icon" v-if="SimPC.isoldrelease" color="warning">warning</v-icon>
+            <div>
+              Updates:
+              <v-icon v-if="SimPC.updates > 10" color="warning">warning</v-icon>
+              <span v-if="SimPC.updates">{{SimPC.updates}} available</span>
+              <span v-else>up-to-date</span>
+            </div>
+            <div>Uptime: {{format(SimPC.uptime)}}</div>
+            <div>
+              Heartbeat:
+              <v-icon v-if="SimPC.inactive" color="warning">warning</v-icon>
+              {{format(SimPC.lastupdate)}}
+            </div>
+            VPN: {{SimPC.vpn ? 'ACTIVE' : 'off'}}
+          </grid-card>
+        </v-layout>
         <span v-else>Keine Daten empfangen</span>
       </v-card-text>
     </v-card>
@@ -18,10 +73,17 @@
 <script>
 import { mapGetters } from 'vuex'
 import SourceView from '@/components/SourceView'
+import GridCard from '@/components/GridCard'
+import UserChip from '@/components/UserChip'
+import CpuLoad from '@/components/CpuLoad'
+import { format } from '@/utils/time'
 
 export default {
   components: {
-    SourceView
+    SourceView,
+    GridCard,
+    UserChip,
+    CpuLoad
   },
   computed: {
     ...mapGetters(['simpcstatus']),
@@ -30,7 +92,19 @@ export default {
     },
     SimPC () {
       return this.simpcstatus.filter(simpc => simpc.number === this.number)[0]
+    },
+    percent_1min () {
+      return Math.ceil((100 * this.SimPC.load_1min) / this.SimPC.cores)
+    },
+    percent_5min () {
+      return Math.ceil((100 * this.SimPC.load_5min) / this.SimPC.cores)
+    },
+    percent_15min () {
+      return Math.ceil((100 * this.SimPC.load_15min) / this.SimPC.cores)
     }
+  },
+  methods: {
+    format
   }
 }
 </script>
