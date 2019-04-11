65d9b1a1ed20ab8ff4431090c730ba1339f6f58b
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2019-04-11 15:50:32 +0200

    add UserChip to Node

--- src/components/Node.vue
+++ src/components/Node.vue
@@ -4,8 +4,7 @@
       <v-card-title>
         <h2>Node {{NodeName}}</h2>
         <v-spacer></v-spacer>
-        <node-action-block :node="NodeName"></node-action-block>
-
+        <node-action-block :node="NodeName"/>
         <v-spacer></v-spacer>
         <source-view v-if="Node" :title="`Node ${NodeName}`" :value="Node"></source-view>
       </v-card-title>
@@ -14,53 +13,66 @@
         <v-layout v-if="Node" row wrap>
           <grid-card title="CPU/RAM">
             <template slot="icon">
-              <v-progress-circular :value="100*Node.CPUAlloc/Node.CPUTot" :color="Node.CPUAlloc == Node.CPUTot ? 'light-blue' : 'green'">
-                {{Node.CPUAlloc}}
-              </v-progress-circular>
+              <v-progress-circular
+                :value="100*Node.CPUAlloc/Node.CPUTot"
+                :color="Node.CPUAlloc == Node.CPUTot ? 'light-blue' : 'green'"
+              >{{Node.CPUAlloc}}</v-progress-circular>
               <cpu-load :load="Number(Node.CPULoad)" :cores="Number(Node.CPUTot)"></cpu-load>
-              <v-progress-circular :value="MemPercent" :color="MemPercent > 90 ? 'red' : 'light-blue'">
-                {{MemPercent}}
-              </v-progress-circular>
-              <v-progress-circular v-for="gres in Gres" :key="gres.ressource" :value="gres.percent" color='light-blue'>
-                {{gres.jobs}}/{{gres.total}}
-              </v-progress-circular>
+              <v-progress-circular
+                :value="MemPercent"
+                :color="MemPercent > 90 ? 'red' : 'light-blue'"
+              >{{MemPercent}}</v-progress-circular>
+              <v-progress-circular
+                v-for="gres in Gres"
+                :key="gres.ressource"
+                :value="gres.percent"
+                color="light-blue"
+              >{{gres.jobs}}/{{gres.total}}</v-progress-circular>
             </template>
-            {{Node.CPUAlloc}} / {{Node.CPUTot}} allocated <span v-if="Node.CPUErr != 0">({{Node.CPUErr}} Err)</span>
-            <span v-for="gres in Gres" :key="gres.ressource">
-              , {{gres.ressource}}: {{gres.jobs}}/{{gres.total}}
-            </span>
+            {{Node.CPUAlloc}} / {{Node.CPUTot}} allocated
+            <span
+              v-if="Node.CPUErr != 0"
+            >({{Node.CPUErr}} Err)</span>
+            <span
+              v-for="gres in Gres"
+              :key="gres.ressource"
+            >, {{gres.ressource}}: {{gres.jobs}}/{{gres.total}}</span>
             <br>
-            <a href="https://en.wikipedia.org/wiki/Load_(computing)">Load</a>: {{Node.CPULoad}} ({{CPULoadPercent}}%)<br>
+            <a href="https://en.wikipedia.org/wiki/Load_(computing)">Load</a>
+            : {{Node.CPULoad}} ({{CPULoadPercent}}%)
+            <br>
             RAM: {{Math.round((Node.RealMemory - Node.FreeMem)/1000)}}/{{Node.RealMemory / 1000}} GB ({{MemPercent}}%)
           </grid-card>
 
           <grid-card title="Users">
             <span v-for="user in Node.users" :key="user">
-              <router-link :to="`/users/${user}`">{{user}}</router-link>
-              &nbsp;
+              <user-chip :login="user"/>
             </span>
           </grid-card>
 
           <grid-card :title="`SLURM ${Node.Version || ''}`">
-            Partitions: {{Node.Partitions}}<br>
-            State: {{Node.States.join(", ")}}<br>
-            <span v-if="Node.Reason">Reason: {{Node.Reason}}<br></span>
+            Partitions: {{Node.Partitions}}
+            <br>
+            State: {{Node.States.join(", ")}}
+            <br>
+            <span v-if="Node.Reason">
+              Reason: {{Node.Reason}}
+              <br>
+            </span>
             Ressources: {{Node.Gres}}
           </grid-card>
 
           <grid-card title="Host">
-            Boot Time: {{Node.BootTime}}<br>
+            Boot Time: {{Node.BootTime}}
+            <br>
             Slurmd Start Time: {{Node.SlurmdStartTime}}
           </grid-card>
-
         </v-layout>
         <span v-else>Keine Daten empfangen</span>
       </v-card-text>
     </v-card>
 
-    <JobList v-if="Node" :title="`${Jobs.length} Jobs`" :items="Jobs">
-    </JobList>
-
+    <JobList v-if="Node" :title="`${Jobs.length} Jobs`" :items="Jobs"></JobList>
   </v-container>
 </template>
 
@@ -70,6 +82,7 @@ import JobList from '@/components/JobList'
 import GridCard from '@/components/GridCard'
 import SourceView from '@/components/SourceView'
 import CpuLoad from '@/components/CpuLoad'
+import UserChip from '@/components/UserChip'
 import NodeActionBlock from '@/components/NodeActionBlock'
 import { uniq, concat } from 'lodash'
 
@@ -84,7 +97,8 @@ export default {
     GridCard,
     SourceView,
     CpuLoad,
-    NodeActionBlock
+    NodeActionBlock,
+    UserChip
   },
   computed: {
     ...mapGetters(['nodestatus', 'is_admin']),
@@ -100,8 +114,7 @@ export default {
       }
 
       const nodeGres = deGres(this.Node.Gres)
-      const jobGres = this.Jobs
-        .filter(job => job.Gres)
+      const jobGres = this.Jobs.filter(job => job.Gres)
         .map(job => job.Gres)
         .map(deGres)
         .reduce((a, b) => {
@@ -117,20 +130,22 @@ export default {
         return undefined
       }
 
-      return allKeys.map(key => ({
-        jobs: jobGres[key] || 0,
-        total: nodeGres[key] || 0,
-        ressource: key })
-      ).map(gres => ({
-        ...gres,
-        percent: Math.round(100 * gres.jobs / (gres.total || 1))
-      }))
+      return allKeys
+        .map(key => ({
+          jobs: jobGres[key] || 0,
+          total: nodeGres[key] || 0,
+          ressource: key
+        }))
+        .map(gres => ({
+          ...gres,
+          percent: Math.round((100 * gres.jobs) / (gres.total || 1))
+        }))
     },
     MemPercent () {
       return 100 - Math.round((100 * this.Node.FreeMem) / this.Node.RealMemory)
     },
     CPULoadPercent () {
-      return Math.round(100 * this.Node.CPULoad / this.Node.CPUTot)
+      return Math.round((100 * this.Node.CPULoad) / this.Node.CPUTot)
     },
     Jobs () {
       return this.Node.jobs || []
