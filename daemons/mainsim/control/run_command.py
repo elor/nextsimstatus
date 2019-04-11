83f7c50be610aeba5f9ab35a844c2e2283349d32
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2019-04-11 09:35:30 +0200

    daemons/mainsim/control: run_command can run as user

--- daemons/mainsim/control/run_command.py
+++ daemons/mainsim/control/run_command.py
@@ -1,7 +1,9 @@
 from subprocess import Popen, PIPE
 
 
-def run_command(command_array, input_stdin=b''):
+def run_command(command_array, input_stdin=b'', user=None):
+    if user:
+        command_array = ['sudo', '-u', user] + command_array
     p = Popen(command_array, stdin=PIPE, stdout=PIPE, stderr=PIPE)
     out, err = p.communicate(input_stdin)
     return p.returncode, out, err
