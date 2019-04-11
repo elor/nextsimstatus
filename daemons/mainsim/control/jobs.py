f8ccff1444c73b4367a44be497f79bddb46043e7
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2019-04-11 11:53:40 +0200

    control: add 'top' and 'throttle' as well as payload evaluation

--- daemons/mainsim/control/jobs.py
+++ daemons/mainsim/control/jobs.py
@@ -2,11 +2,13 @@ from run_command import run_command
 import config
 
 COMMAND_BY_ACTION = {
-    'test': lambda jobs: ['squeue', '--jobs', jobs, '--format=%i\t%u', '--noheader', '--states=PENDING,RUNNING,SUSPENDED,COMPLETED,CANCELLED,FAILED,TIMEOUT,NODE_FAIL,PREEMPTED,BOOT_FAIL,DEADLINE,OUT_OF_MEMORY,COMPLETING,CONFIGURING,RESIZING,REVOKED,SPECIAL_EXIT'],
-    'abort': lambda jobs: ['scancel', jobs],
-    'hold': lambda jobs: ['scontrol', 'hold', jobs],
-    'release': lambda jobs: ['scontrol', 'release', jobs],
-    'requeue': lambda jobs: ['scontrol', 'requeue', jobs]
+    'test': lambda jobs, _=None: ['squeue', '--jobs', jobs, '--format=%i\t%u', '--noheader', '--states=PENDING,RUNNING,SUSPENDED,COMPLETED,CANCELLED,FAILED,TIMEOUT,NODE_FAIL,PREEMPTED,BOOT_FAIL,DEADLINE,OUT_OF_MEMORY,COMPLETING,CONFIGURING,RESIZING,REVOKED,SPECIAL_EXIT'],
+    'abort': lambda jobs, _=None: ['scancel', jobs],
+    'hold': lambda jobs, _=None: ['scontrol', 'hold', jobs],
+    'release': lambda jobs, _=None: ['scontrol', 'release', jobs],
+    'requeue': lambda jobs, _=None: ['scontrol', 'requeue', jobs],
+    'top': lambda jobs, _=None: ['scontrol', 'top', jobs],
+    'throttle': lambda jobs, count: ['scontrol', 'update', 'JobId={}'.format(jobs), 'ArrayTaskThrottle={}'.format(count)],
 }
 
 
@@ -27,12 +29,13 @@ def test(jobs, user, is_admin):
     return out
 
 
-def command(action, jobs):
-    return COMMAND_BY_ACTION[action](jobs)
+def command(action, jobs, payload=None):
+    return COMMAND_BY_ACTION[action](jobs, payload)
 
 
-def control(action, jobs, user=None):
-    return_code, out, err = run_command(command(action, jobs), user=user)
+def control(action, jobs, user=None, payload=None):
+    return_code, out, err = run_command(
+        command(action, jobs, payload), user=user)
     if return_code:
         raise RuntimeError(
             'NonZero Return Code {}: {}'.format(return_code, err))
@@ -42,4 +45,4 @@ def control(action, jobs, user=None):
 if __name__ == "__main__":
     from sys import argv, exit
     cmd, jobs, user = argv
-    print test(jobs, user)
+    print test(jobs, user, user == config.ADMIN_GROUP)
