d3263fc4b35dbaf43bd21b135db55de2daee39a3
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2019-07-16 16:03:57 +0200

    Remove material design icons in favor of fontawesome

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,7 +3,6 @@ import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.use(Vuetify, {
   iconfont: 'fa'
