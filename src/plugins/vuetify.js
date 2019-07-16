4aedeef548802e65da34555e09d0d3576016c573
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2019-07-16 14:58:03 +0200

    Default to Fontawesome fonts

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import '@fortawesome/fontawesome-free/css/all.css' // Ensure you are using css-loader
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.use(Vuetify, {
-  iconfont: 'md'
+  iconfont: 'fa'
 })
