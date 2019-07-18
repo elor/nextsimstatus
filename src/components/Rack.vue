c6dd17cf496ad014b6275dd3ec53f40c52dca02e
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2019-07-18 15:19:25 +0200

    Use thermometer icons for rack temperatures

--- src/components/Rack.vue
+++ src/components/Rack.vue
@@ -1,13 +1,12 @@
 <template>
   <p>
     {{rack.name}}
-    <br>
+    <br />
     <rack-temperature :values="rack.temperatures.supply_water" title="supply_water"></rack-temperature>
     <rack-temperature :values="rack.temperatures.supply_air" title="supply_air"></rack-temperature>
     <rack-temperature :values="rack.temperatures.return_air" title="return_air"></rack-temperature>
     <rack-temperature :values="rack.temperatures.return_water" title="return_water"></rack-temperature>
     °C
-
     {{rack.cooling_capacity}} kW
     @
     {{rack.water_flow}} m³/h
--- src/components/RackTemperature.vue
+++ src/components/RackTemperature.vue
@@ -1,7 +1,11 @@
 <template>
-  <v-progress-circular :value="percent" :color="color">
-    {{text}}
-  </v-progress-circular>
+  <span>
+    <v-icon small v-if="isnan" class="mr-3">fa-question</v-icon>
+    <template v-else>
+      <v-icon :color="color">fa-thermometer-{{icon}}</v-icon>
+      <span class="mr-2" :class="{[`${color}--text`]:true}">{{Math.round(value)}}</span>
+    </template>
+  </span>
 </template>
 
 <script>
@@ -19,6 +23,19 @@ export default {
         return 'green'
       }
     },
+    icon () {
+      if (this.value >= this.max) {
+        return 'full'
+      } else if (this.value >= this.high) {
+        return 'three-quarters'
+      } else if (this.value >= this.low) {
+        return 'half'
+      } else if (this.value >= this.min) {
+        return 'quarter'
+      } else {
+        return 'empty'
+      }
+    },
     value () {
       return this.values.val
     },
@@ -27,6 +44,9 @@ export default {
     },
     max () {
       return this.values.max
+    },
+    isnan () {
+      return isNaN(this.value)
     },
     percent () {
       return 100 * (this.value - this.min) / (this.max - this.min)
