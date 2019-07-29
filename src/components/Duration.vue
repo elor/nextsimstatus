cb16cbd3e7f899b3dfeb5c3db4b8adbc175d3393
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2019-07-29 12:17:43 +0200

    Duration.vue handles SLURM duration format (as iso)

--- src/components/Duration.vue
+++ src/components/Duration.vue
@@ -28,6 +28,8 @@ const SHORT_TIME_FORMAT = {
   month: '2-digit'
 }
 
+const RUNTIME_REGEX = /^(?:(\d+)-)?(\d\d):(\d\d):(\d\d)$/
+
 export default {
   props: {
     seconds: {
@@ -48,20 +50,37 @@ export default {
     }
   },
   computed: {
+    iso_seconds () {
+      if (this.iso && RUNTIME_REGEX.test(this.iso)) {
+        const [days, hours, minutes, seconds] = this.iso.match(RUNTIME_REGEX).slice(1).map(str => Number(str || 0))
+        return seconds + 60 * (minutes + 60 * (hours + 24 * days))
+      }
+      return undefined
+    },
+    iso_date () {
+      if (this.iso && this.iso_seconds === undefined) {
+        return new Date(this.iso)
+      }
+      return undefined
+    },
     formatted () {
-      if (this.seconds) {
+      if (this.seconds !== undefined) {
         return format(this.seconds)
-      } else if (this.iso) {
-        return new Date(this.iso).toLocaleString(LOCALE, SHORT_TIME_FORMAT)
+      } else if (this.iso_seconds !== undefined) {
+        return this.iso
+      } else if (this.iso_date) {
+        return this.iso_date.toLocaleString(LOCALE, SHORT_TIME_FORMAT)
       }
       return undefined
     },
     hovertext () {
       if (this.since) {
-        if (this.seconds) {
+        if (this.seconds !== undefined) {
           return new Date(new Date() - 1000 * this.seconds).toLocaleString(LOCALE, LONG_TIME_FORMAT)
-        } else if (this.iso) {
-          return new Date(this.iso).toLocaleString(LOCALE, LONG_TIME_FORMAT)
+        } else if (this.iso_seconds !== undefined) {
+          return new Date(new Date() - 1000 * this.iso_seconds).toLocaleString(LOCALE, LONG_TIME_FORMAT)
+        } else if (this.iso_date) {
+          return this.iso_date.toLocaleString(LOCALE, LONG_TIME_FORMAT)
         }
       }
 
