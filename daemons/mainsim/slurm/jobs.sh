a7f6151fb38fdf6e96da1ed07963a62c128ba92b
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2019-09-18 10:19:46 +0200

    lazy-match scontrol job key-value pairs

--- daemons/mainsim/slurm/jobs.sh
+++ daemons/mainsim/slurm/jobs.sh
@@ -6,6 +6,6 @@ jobs=$(scontrol show jobs --oneliner)
 
 grep "No jobs" <<< "$jobs" &>/dev/null && { echo '[]'; exit 0; }
 
-perl -pe 's/(\S+)=(.*?)(?= \S+=|$)/"\1": "\2", /g'  <<< "$jobs" \
+perl -pe 's/(\S+?)=(.*?)(?= \S+=|$)/"\1": "\2", /g'  <<< "$jobs" \
     | sed -r -e 's/^(.+),\s*$/{\1},/' -e '1s/^/[/' -e '$s/},$/}]/' \
     | ./jobfilter.py
