2cedfbbd2ebe03688ac1e55d923608d236c61a38
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2020-02-13 07:53:12 +0100

    add GroupChip, which uses groupcolor

---
+++ src/components/GroupChip.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-chip small :color="color" :dark="isDark">{{group}}</v-chip>
+</template>
+
+<script>
+import groupcolor from '../utils/groupcolor'
+import { isDark } from '../utils/color'
+
+export default {
+  props: {
+    group: String
+  },
+  computed: {
+    color () {
+      return this.groupcolor(this.group)
+    },
+    isDark () {
+      return isDark(this.color)
+    }
+  },
+  methods: {
+    groupcolor
+  }
+}
+</script>
