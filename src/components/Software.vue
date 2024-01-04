88354f21ddb93bcf8fa0afe37809d20f0c6f8421
Erik E. Lorenz <erik@tuvero.de>
2024-01-04 10:50:23 +0100

    Software cards: remove LAM, dfnconf

--- src/components/Software.vue
+++ src/components/Software.vue
@@ -3,98 +3,56 @@
     <h2>Werkzeuge für die Wissenschaft</h2>
     <v-container fluid>
       <v-layout wrap row>
-        <SoftwareCard
-          title="Gitlab"
-          hidetitle
-        >Lege deinen Quellcode ab, überwache deine Builds und tracke deine Bugs</SoftwareCard>
+        <SoftwareCard title="Gitlab" hidetitle>Lege deinen Quellcode ab, überwache deine Builds und tracke deine Bugs
+        </SoftwareCard>
 
-        <SoftwareCard
-          title="Jupyter"
-          hidetitle
-        >Arbeite interaktiv mit Python an wissenschaftlichen Daten und visualisiere sie im Browser</SoftwareCard>
+        <SoftwareCard title="Jupyter" hidetitle>Arbeite interaktiv mit Python an wissenschaftlichen Daten und visualisiere
+          sie im Browser</SoftwareCard>
 
-        <SoftwareCard
-          title="TUC-Wiki"
-          href="https://wiki.tu-chemnitz.de/ENAS/BeolSimulation"
-          src="foswiki"
-        >Tausche Wissen und Erfahrungen mit den anderen Simulanten aus</SoftwareCard>
+        <!--
+        <SoftwareCard title="TUC-Wiki" href="https://wiki.tu-chemnitz.de/ENAS/BeolSimulation" src="foswiki">Tausche Wissen
+          und Erfahrungen mit den anderen Simulanten aus</SoftwareCard>
+        -->
 
-        <SoftwareCard
-          title="scholar"
-          hidetitle
-          href="https://scholar.google.com/"
-        >Suche nach Publikationen und Autoren</SoftwareCard>
+        <SoftwareCard title="scholar" hidetitle href="https://scholar.google.com/">Suche nach Publikationen und Autoren
+        </SoftwareCard>
       </v-layout>
     </v-container>
 
     <h2>TUC, DFN & Co.</h2>
     <v-container fluid>
       <v-layout wrap row>
-        <SoftwareCard
-          title="BigBlueButton"
-          href="https://webroom.hrz.tu-chemnitz.de"
-        >Zuverlässige Videokonferenz- und Screensharing Lösung der Uni</SoftwareCard>
+        <SoftwareCard title="BigBlueButton" href="https://webroom.hrz.tu-chemnitz.de">Zuverlässige Videokonferenz- und
+          Screensharing Lösung der Uni</SoftwareCard>
 
-        <SoftwareCard
-          usergroup="enas"
-          title="ENAS-Intranet"
-          src="enas"
-          href="http://intranet.enas.fraunhofer.de"
-        >
+        <SoftwareCard usergroup="enas" title="ENAS-Intranet" src="enas" href="http://intranet.enas.fraunhofer.de">
           Für ENAS-Angestellte:
           <br />Suche Vorlagen, gesetzliche Regelungen und Formulare.
         </SoftwareCard>
 
-        <SoftwareCard
-          usergroup="enas"
-          title="FhG-Intranet"
-          src="fraunhofer"
-          href="https://info.fraunhofer.de"
-        >
+        <SoftwareCard usergroup="enas" title="FhG-Intranet" src="fraunhofer" href="https://info.fraunhofer.de">
           Für Fraunhofer-Angestellte:
           <br />Das deutschlandweite Fraunhofer-Intranet. Für uns nur
           <b>via VPN</b> verfügbar.
         </SoftwareCard>
 
-        <SoftwareCard
-          title="Terminplaner"
-          href="https://terminplaner4.dfn.de/"
-        >Stimme Termine ab - Doodle vom Deutschen Forschungsnetz</SoftwareCard>
-        <SoftwareCard
-          title="dfnconf"
-          hidetitle
-          href="http://www.conf.dfn.de/"
-        >DFN Konferenztelefonie - Der Konferenzdienst im Deutschen Forschungsnetz</SoftwareCard>
+        <SoftwareCard title="Terminplaner" href="https://terminplaner4.dfn.de/">Stimme Termine ab - Doodle vom Deutschen
+          Forschungsnetz</SoftwareCard>
 
-        <SoftwareCard
-          title="TUC-Cloud"
-          href="https://tuc.cloud/"
-        >Tausche Dateien zwischen Geräten aus und teile sie mit Anderen</SoftwareCard>
+        <SoftwareCard title="TUC-Cloud" href="https://tuc.cloud/">Tausche Dateien zwischen Geräten aus und teile sie mit
+          Anderen</SoftwareCard>
 
-        <SoftwareCard
-          usergroup="enas"
-          title="FhG-Cloud"
-          src="owncloud"
-          href="https://owncloud.fraunhofer.de/"
-        >Tausche Dateien zwischen Geräten aus und teile sie mit Anderen. Ist ständig kaputt.</SoftwareCard>
+        <SoftwareCard usergroup="enas" title="FhG-Cloud" src="owncloud" href="https://owncloud.fraunhofer.de/">Tausche
+          Dateien zwischen Geräten aus und teile sie mit Anderen. Ist ständig kaputt.</SoftwareCard>
 
-        <SoftwareCard
-          title="MAIN-Webseite"
-          src="tuc"
-          href="https://www.tu-chemnitz.de/main/"
-        >Die offizielle Infoseite rund um unser MAIN-Gebäude</SoftwareCard>
+        <SoftwareCard title="MAIN-Webseite" src="tuc" href="https://www.tu-chemnitz.de/main/">Die offizielle Infoseite
+          rund um unser MAIN-Gebäude</SoftwareCard>
 
-        <SoftwareCard
-          title="URZ IdM"
-          href="https://idm.hrz.tu-chemnitz.de/"
-          src="tuc"
-        >Verwalte deinen Nutzer-Ressourcen am Universitätsrechenzentrum</SoftwareCard>
+        <SoftwareCard title="URZ IdM" href="https://idm.hrz.tu-chemnitz.de/" src="tuc">Verwalte deinen Nutzer-Ressourcen
+          am Universitätsrechenzentrum</SoftwareCard>
 
-        <SoftwareCard
-          title="Click2Dial"
-          href="https://voip-service.hrz.tu-chemnitz.de/service/dial.html"
-          src="tuc"
-        >Wähle Rufnummern auf deinem Bürotelefon direkt über deinen Browser</SoftwareCard>
+        <SoftwareCard title="Click2Dial" href="https://voip-service.hrz.tu-chemnitz.de/service/dial.html" src="tuc">Wähle
+          Rufnummern auf deinem Bürotelefon direkt über deinen Browser</SoftwareCard>
       </v-layout>
     </v-container>
 
@@ -111,27 +69,16 @@
           <br />SimAdmins haben Superkräfte
         </SoftwareCard>
 
-        <SoftwareCard title="LAM" usergroup="simadmin" href="http://mainsimweb.etit.tu-chemnitz.de:8741/lam">
-          LDAP Account Manager (Admins only):
-          <br />Richte neue Benutzer ein und ändere Passwörter
+        <SoftwareCard title="xcat-doc" src="xcat" usergroup="simadmin">Dokumentation des XCAT-Clustermanagers (Manpages)
         </SoftwareCard>
-
-        <SoftwareCard
-          title="xcat-doc"
-          src="xcat"
-          usergroup="simadmin"
-        >Dokumentation des XCAT-Clustermanagers (Manpages)</SoftwareCard>
 
         <SoftwareCard title="Ganglia" hidetitle usergroup="simadmin">
           Überwache CPU, RAM und Festplatten aller Rechenknoten.
           (deprecated)
         </SoftwareCard>
 
-        <SoftwareCard
-          title="Nagios"
-          hidetitle
-          usergroup="simadmin"
-        >Überwache den Systemzustand und die Daemons aller Knoten
+        <SoftwareCard title="Nagios" hidetitle usergroup="simadmin">Überwache den Systemzustand und die Daemons aller
+          Knoten
           (deprecated)
         </SoftwareCard>
         <SoftwareCard title="GraphQL" usergroup="simadmin">Direktzugriff auf MainSimStatus-Datenbank</SoftwareCard>
@@ -141,22 +88,15 @@
     <h2>Spielereien</h2>
     <v-container fluid>
       <v-layout wrap row>
-        <SoftwareCard
-          title="Periodyfier"
-          href="/#/apps/periodyfier"
-        >Erstelle bunten Text aus Element-Symbolen</SoftwareCard>
+        <SoftwareCard title="Periodyfier" href="/#/apps/periodyfier">Erstelle bunten Text aus Element-Symbolen
+        </SoftwareCard>
 
-        <SoftwareCard
-          title="Vuetify"
-          href="https://vuetifyjs.com/en/components/api-explorer"
-          usergroup="simadmin"
-        >Direktlink zur Dokumentation des Frameworks, auf dem MainSimStatus basiert</SoftwareCard>
+        <SoftwareCard title="Vuetify" href="https://vuetifyjs.com/en/components/api-explorer" usergroup="simadmin">
+          Direktlink zur Dokumentation des Frameworks, auf dem MainSimStatus basiert</SoftwareCard>
 
-        <SoftwareCard
-          title="Deine Ergänzung"
+        <SoftwareCard title="Deine Ergänzung"
           href="https://mainsim.etit.tu-chemnitz.de:4485/simadmin/mainsimstatus/blob/master/src/components/Software.vue"
-          src="simcrew"
-        >Du vermisst etwas? Dann füge deinen Beitrag hinzu</SoftwareCard>
+          src="simcrew">Du vermisst etwas? Dann füge deinen Beitrag hinzu</SoftwareCard>
       </v-layout>
     </v-container>
   </div>
@@ -172,5 +112,4 @@ export default {
 }
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
