881cc522970e50c6cebf1c06002ae1b5bcd9b9ba
Erik E. Lorenz <erik@tuvero.de>
2024-05-31 13:23:10 +0200

    update icons

--- public/img/icons/src/convert.sh
+++ public/img/icons/src/convert.sh
@@ -2,31 +2,31 @@
 
 set -e -u
 
-toPNG(){
+toPNG() {
 	size="$1"
 	source="$2"
 	destination="$3"
 
-	inkscape -z -e "$destination" -w "$size" -h "$size" "$source"
+	convert "$source" -verbose -trim +repage -resize ${size}x${size} "$destination"
 }
 
-srcsvg=mainsim-logo.svg
+srcimg=nextsim-logo-original.png
 
-toPNG 128 $srcsvg mainsim-logo.png
+toPNG 128 $srcimg nextsim-logo.png
 
-toPNG 192 $srcsvg ../android-chrome-192x192.png
-toPNG 512 $srcsvg ../android-chrome-512x512.png
-toPNG 120 $srcsvg ../apple-touch-icon-120x120.png
-toPNG 152 $srcsvg ../apple-touch-icon-152x152.png
-toPNG 180 $srcsvg ../apple-touch-icon-180x180.png
-toPNG 60 $srcsvg ../apple-touch-icon-60x60.png
-toPNG 76 $srcsvg ../apple-touch-icon-76x76.png
-toPNG 180 $srcsvg ../apple-touch-icon.png
-toPNG 16 $srcsvg ../favicon-16x16.png
-toPNG 32 $srcsvg ../favicon-32x32.png
-toPNG 144 $srcsvg ../msapplication-icon-144x144.png
-toPNG 150 $srcsvg ../mstile-150x150.png
+toPNG 192 $srcimg ../android-chrome-192x192.png
+toPNG 512 $srcimg ../android-chrome-512x512.png
+toPNG 120 $srcimg ../apple-touch-icon-120x120.png
+toPNG 152 $srcimg ../apple-touch-icon-152x152.png
+toPNG 180 $srcimg ../apple-touch-icon-180x180.png
+toPNG 60 $srcimg ../apple-touch-icon-60x60.png
+toPNG 76 $srcimg ../apple-touch-icon-76x76.png
+toPNG 180 $srcimg ../apple-touch-icon.png
+toPNG 16 $srcimg ../favicon-16x16.png
+toPNG 32 $srcimg ../favicon-32x32.png
+toPNG 144 $srcimg ../msapplication-icon-144x144.png
+toPNG 150 $srcimg ../mstile-150x150.png
 
-convert -verbose -scale 16x16 $srcsvg ../../../favicon.ico
+convert -verbose -scale 16x16 $srcimg ../../../favicon.ico
 
-cp $srcsvg ../safari-pinned-tab.svg
+# cp $srcimg ../safari-pinned-tab.svg
