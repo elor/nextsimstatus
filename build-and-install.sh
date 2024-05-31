dedd3dced692570ca61bc1a5ca2b2a056480ee59
Erik E. Lorenz <erik@tuvero.de>
2024-05-31 13:23:27 +0200

    add docker-based daemons to build-and-install.sh

--- build-and-install.sh
+++ build-and-install.sh
@@ -4,6 +4,17 @@ set -e -u
 
 cd "$(dirname "$(readlink -f "$0")")"
 
+# webpage
 podman-compose run nextsimstatus npm run build
 
-rsync --delete ./dist/ /srv/www/status
+rsync -rv --delete ./dist/ /srv/www/status
+
+# daemons
+rsync -rv --delete ./daemons/ /opt/nextsimstatus
+
+(
+	cd /opt/nextsimstatus/
+	podman-compose up --build --detach
+)
+
+echo "Success"
