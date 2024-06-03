d2dea092c0966c54c2bc0082a89abc0989ae2bec
Erik E. Lorenz <erik@tuvero.de>
2024-06-03 09:00:05 +0200

    reorder build-and-install

--- build-and-install.sh
+++ build-and-install.sh
@@ -4,14 +4,16 @@ set -e -u
 
 cd "$(dirname "$(readlink -f "$0")")"
 
-# webpage
-podman-compose run nextsimstatus npm run build
-
-rsync -rv --delete ./dist/ /srv/www/status
-
-# daemons
+# copy daemons over
 rsync -rv --delete ./daemons/ /opt/nextsimstatus
 
+# build single page application
+podman-compose run nextsimstatus npm run build
+
+# deploy single page application
+rsync -rv --delete ./dist/ /srv/www/status
+
+# restart docker daemons if necessary
 (
 	cd /opt/nextsimstatus/
 	podman-compose up --build --detach -t 1
