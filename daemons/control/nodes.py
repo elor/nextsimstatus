faf07eea6e9acb2fe018c715d04550a007bfc7a3
Erik E. Lorenz <erik@tuvero.de>
2024-06-03 09:54:04 +0200

    proper string conversions in nodes.py

--- daemons/control/nodes.py
+++ daemons/control/nodes.py
@@ -4,30 +4,29 @@ import config
 
 def test(nodes):
     if not nodes:
-        raise RuntimeError('No nodes defined')
+        raise RuntimeError("No nodes defined")
 
     return_code, out, err = run_command(["/opt/xcat/bin/nodels", nodes])
     if return_code:
-        raise RuntimeError(
-            'NonZero Return Code {}: {}'.format(return_code, err))
+        raise RuntimeError("NonZero Return Code {}: {}".format(return_code, err))
     if not out:
-        raise RuntimeError('no nodes found')
-    return out
+        raise RuntimeError("no nodes found")
+    return out.decode("utf-8")
 
 
 def command(action, nodes):
-    return ['nextsim', action, nodes]
+    return ["nextsim", action, nodes]
 
 
 def control(action, nodes):
     return_code, out, err = run_command(command(action, nodes))
     if return_code:
-        raise RuntimeError(
-            'NonZero Return Code {}: {}'.format(return_code, err))
-    return out
+        raise RuntimeError("NonZero Return Code {}: {}".format(return_code, err))
+    return out.decode("utf-8")
 
 
 if __name__ == "__main__":
-    from sys import argv, exit
+    from sys import argv
+
     cmd, nodes = argv
     print(test(nodes))
