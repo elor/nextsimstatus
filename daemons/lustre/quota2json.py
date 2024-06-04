2aca35b0d6f0c80a6298530f680f6eb35ab13be1
Erik E. Lorenz <erik@tuvero.de>
2024-06-04 09:36:11 +0200

    add df to lustre, and run `nextsim quota` ourselves

--- daemons/lustre/quota2json.py
+++ daemons/lustre/quota2json.py
@@ -1,9 +1,8 @@
 import re
-import sys
 import json
+import subprocess
 
-data = sys.stdin.read()
-
+data = subprocess.run(["nextsim", "quota"], capture_output=True, text=True).stdout
 lines = data.strip().split("\n")
 
 headers = None
@@ -32,6 +31,33 @@ for line in lines:
         for header, value in zip(headers, values)
         if values not in ["-", ""]
     }
+
+# Execute df -k /home command to get disk usage in KB
+lustre_mounts = [
+    line[1]
+    for line in (line.split() for line in open("/proc/mounts"))
+    if line[2] == "lustre"
+]
+df_output = subprocess.run(
+    ["df", "-k"] + lustre_mounts, capture_output=True, text=True
+).stdout
+df_lines = df_output.strip().split("\n")
+
+# Parse df output and add it to the 'disk' category
+df_headers = df_lines[0].split()
+output["df"] = {}
+
+for line in df_lines[1:]:
+    df_values = line.split()
+    mount = df_values[-1]
+    print(df_values, mount)
+
+    disk_info = {}
+
+    for header, value in zip(df_headers, df_values):
+        disk_info[header] = int(value) if value.isdigit() else value
+
+    output["df"][mount] = disk_info
 
 json_output = json.dumps(output, indent=2)
 print(json_output)
