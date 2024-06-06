2f50da8c8a3a9d8d6f816ee9e568bcad32a53dcb
Erik E. Lorenz <erik@tuvero.de>
2024-06-06 10:40:13 +0200

    weitere periodinator examples

--- src/assets/periodinator-examples.js
+++ src/assets/periodinator-examples.js
@@ -1,16 +1,13 @@
 export default [
-  'Und so schreiten wir ohne Pause sicheren Fußes gen Promotion',
   'SimCrew',
-  'In Cooperation with Prof Schreiber',
   'Physik ohne Sinn und Verstand',
   'Periodyfier',
-  'Arsen\nCer\nKupfer\nKrypton\nNiob\nNeon\nOganeson\nPlatin\nTitan\nTenness\nUran\nXenon',
-  'Carbon\nNeon\nSilicon\nPhosphorus\nCopper\nArsenic\nKrypton\nSilver\nXenon\nBismuth\nAstatine\nTenessine\nOganesson',
+  'Arsen\nCer\nKupfer\nKrypton\nNiob\nNeon\nOganeson\nPlatin\nTitan\nTenness\nUran\nXenon\nCarbon\nNeon\nSilicon\nPhosphorus\nCopper\nArsenic\nKrypton\nSilver\nXenon\nBismuth\nAstatine\nTenessine\nOganesson',
   'This site wastes huge amounts of brainpower',
-  'Haufen\nKaufen\nLaufen\nRaufen\nSaufen\nTaufen',
   'Pausenfunktionsseite',
   'Brokkoliauflauf',
   'I watch rainbows',
   'Find more unusual sentences',
-  'This page has no serious use'
+  'This page has no serious use',
+  'ich bin ungern gegangen'
 ]
