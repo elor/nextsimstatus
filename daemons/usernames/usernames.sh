12273fe4035a6f697814424022316afe844c1191
Erik E. Lorenz <erik@tuvero.de>
2024-06-07 09:20:45 +0200

    usernames daemon: remove '_' placeholder entry

--- daemons/usernames/usernames.sh
+++ daemons/usernames/usernames.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cat <<EOF | jq 'to_entries | map_values({key: .key, value: {Login: .key, FullName: .value}}) | from_entries'
+cat <<EOF | jq 'to_entries | map(select(.key != "_")) | map_values({key: .key, value: {Login: .key, FullName: .value}}) | from_entries'
 {
 $(/usr/local/sbin/nextsim user names | sed -e 's/^\(\S\+\) \(.*\)$/"\1": "\2",/')
 "_":"_" }
