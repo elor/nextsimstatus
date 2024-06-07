8f3faa469e3c627c8a374caabe8a8cafe6c2edee
Erik E. Lorenz <erik@tuvero.de>
2024-06-07 09:36:24 +0200

    add hover text to UserChips

--- src/components/UserChip.vue
+++ src/components/UserChip.vue
@@ -1,17 +1,17 @@
 <template>
-  <v-chip
-    v-ripple
-    small
-    :color="color"
-    :disabled="disabled"
-    :dark="isDark"
-    @click="$router.push(`/users/${login}`)"
-  >{{login}}</v-chip>
+  <v-tooltip bottom>
+    <template v-slot:activator="{ on }">
+      <v-chip v-ripple small :color="color" :disabled="disabled" :dark="isDark" @click="$router.push(`/users/${login}`)"
+        v-on="on">{{ login }}</v-chip>
+    </template>
+    <span>{{ fullname }}</span>
+  </v-tooltip>
 </template>
 
 <script>
 import usercolor from '../utils/usercolor'
 import { isDark } from '../utils/color'
+import { mapState } from 'vuex'
 
 export default {
   props: {
@@ -22,11 +22,16 @@ export default {
     }
   },
   computed: {
+    ...mapState(['users']),
     color() {
       return usercolor(this.login)
     },
     isDark() {
       return isDark(this.color)
+    },
+    fullname() {
+      console.log(this.users)
+      return this.users[this.login]?.FullName || `${this.login} (cannot find full name)`
     }
   }
 }
