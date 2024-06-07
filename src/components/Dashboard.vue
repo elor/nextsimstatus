5d7bbb1478d8c9b2a0ea95542d1dba68c8fd4366
Erik E. Lorenz <erik@tuvero.de>
2024-06-07 09:30:03 +0200

    Dashboard: add fullname hover text to users

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,11 +1,11 @@
 <template>
   <v-container fluid>
-    <v-alert type="warning" dismissible v-if="motd">/etc/motd:<pre>{{motd}}</pre></v-alert>
+    <v-alert type="warning" dismissible v-if="motd">/etc/motd:<pre>{{ motd }}</pre></v-alert>
 
     <v-card class="mb-3">
       <v-card-title>
         <v-btn to="/nodes">Nodes</v-btn>
-        <v-btn to="/jobs">{{jobstatus.length}} Jobs</v-btn>
+        <v-btn to="/jobs">{{ jobstatus.length }} Jobs</v-btn>
       </v-card-title>
       <v-card-text>
         <v-layout row wrap>
@@ -17,13 +17,11 @@
             <div class="mb-3 mt-3" v-if="!partitionstatus || partitionstatus.length === 0">
               Couldn't load node partitions from SLURM
             </div>
-            <div class="mb-3 mr-3" v-for="partition in partitionstatus" :key="partition.PartitionName" style="float:left">
-              <v-progress-circular
-                slot="icon"
-                :value="100*partition.CPUAlloc/partition.CPUTot"
-                :color="partition.CPUAlloc == partition.CPUTot ? 'light-blue' : 'green'"
-              >
-                <b>{{partition.PartitionName}}</b>
+            <div class="mb-3 mr-3" v-for="partition in partitionstatus" :key="partition.PartitionName"
+              style="float:left">
+              <v-progress-circular slot="icon" :value="100 * partition.CPUAlloc / partition.CPUTot"
+                :color="partition.CPUAlloc == partition.CPUTot ? 'light-blue' : 'green'">
+                <b>{{ partition.PartitionName }}</b>
               </v-progress-circular>
               <node-load v-for="node in partition.Nodes" :key="node.NodeName" :node="node"></node-load>
             </div>
@@ -47,38 +45,32 @@
       <v-card-text>
         <v-layout row wrap>
           <v-flex lg3 md4 sm6 xs12 v-for="user in users_sorted" :key="user.UserName">
-            <v-hover v-slot:default="{ hover }">
-              <v-card
-                class="ma-1 pa-1"
-                height="70"
-                :to="`/users/${user.UserName}`"
-                :elevation="hover ? 8 : 2"
-                v-ripple
-              >
-                <v-layout>
-                  <v-flex xs4>
-                    <user-chip :login="user.UserName" />
-                  </v-flex>
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on }">
+                <v-card class="ma-1 pa-1" height="70" :to="`/users/${user.UserName}`" v-ripple v-on="on">
+                  <v-layout>
+                    <v-flex xs4>
+                      <user-chip :login="user.UserName" />
+                    </v-flex>
 
-                  <v-flex xs4>
-                    <div>{{user.NumCPUs}} Cores</div>
-                  </v-flex>
-                </v-layout>
-                <div v-if="user.Jobs.length">
-                  Jobs:
-                  <span
-                    v-for="[key, value] in Object.entries(user.JobCount).filter(a => a[1])"
-                    :key="key"
-                    class="mx-2"
-                  >
-                    <v-icon color="warning" v-if="key=='Failed'">fa-exclamation-triangle</v-icon>
-                    {{value}}
-                    {{key}}
-                  </span>
-                </div>
-                <i v-else>Keine Jobs</i>
-              </v-card>
-            </v-hover>
+                    <v-flex xs4>
+                      <div>{{ user.NumCPUs }} Cores</div>
+                    </v-flex>
+                  </v-layout>
+                  <div v-if="user.Jobs.length">
+                    Jobs:
+                    <span v-for="[key, value] in Object.entries(user.JobCount).filter(a => a[1])" :key="key"
+                      class="mx-2">
+                      <v-icon color="warning" v-if="key == 'Failed'">fa-exclamation-triangle</v-icon>
+                      {{ value }}
+                      {{ key }}
+                    </span>
+                  </div>
+                  <i v-else>Keine Jobs</i>
+                </v-card>
+              </template>
+              <span>{{ user.FullName }}</span>
+            </v-tooltip>
           </v-flex>
         </v-layout>
       </v-card-text>
